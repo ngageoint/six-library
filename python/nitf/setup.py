7e1008a90d3ecf214fc5748131b337b4703ce9d5
Tom Zellman <tzellman@github.com>
2008-03-04 01:16:47 +0000

    added python bindings for 1.5
    git-svn-id: http://svn.code.sf.net/p/nitro-nitf/code/trunk@43 7dc3ea37-dbd3-4c13-bb9a-7ccd57e75167

---
+++ python/nitf/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+import os, glob, sys, shutil
+
+files = [os.path.normpath(f).replace('\\', '/') for f in \
+              glob.glob('src/main/python/*.dll') + \
+              glob.glob('src/main/python/nitf/*.so') + \
+              glob.glob('src/main/python/nitf/*.pyd')]
+
+
+out_dir = 'build/lib/nitf'
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
+for f in files:
+    shutil.copy(f, out_dir)
+
+
+setup(
+    name = 'NITRO',
+    version = '1.5-RC3',
+    url = 'http://nitro-nitf.sourceforge.net',
+    author = 'tzellman',
+    author_email = 'tzellman@users.sourceforget.net',
+    description = 'Python bindings for the NITRO project',
+    packages = ['nitf'],
+    package_dir = {'':'src/main/python'},
+    license = "LGPL",
+    scripts=['src/test/python/nitf_extract.py', 'src/test/python/nitf_print.py'],
+)
