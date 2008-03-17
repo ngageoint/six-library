d9c5a3587fa6be27a91be5565a011e08a481e3d1
gojira_1 <gojira_1@github.com>
2008-03-17 18:55:02 +0000

    Added skeleton for XPath traversal of XMLTRE
    git-svn-id: http://svn.code.sf.net/p/nitro-nitf/code/branches/NITRO-2.0@104 7dc3ea37-dbd3-4c13-bb9a-7ccd57e75167

--- external/example-plugins/source/XMLTRE.c
+++ external/example-plugins/source/XMLTRE.c
@@ -28,6 +28,8 @@
 /* This example uses libxml2.  It requires that, which in turn requires iconv and zlib */
 #include <libxml/tree.h>
 #include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
 
 NITF_CXX_GUARD
 
@@ -55,15 +57,15 @@ typedef struct _XMLTREData
 NITFPRIV(XMLTREData*) XMLTREData_construct(nitf_Error* error)
 {
     XMLTREData* data = (XMLTREData*)malloc(sizeof(XMLTREData));
-	if (!data) 
-	{ 
-		nitf_Error_init(error, NITF_STRERROR(NITF_ERRNO), NITF_CTXT, NITF_ERR_MEMORY);
-		return NULL;
-	}
-	data->memorySize = -1;
-	data->memory = NULL;
-	data->dirty = 0;
-	return data;
+    if (!data) 
+    { 
+	nitf_Error_init(error, NITF_STRERROR(NITF_ERRNO), NITF_CTXT, NITF_ERR_MEMORY);
+	return NULL;
+    }
+    data->memorySize = -1;
+    data->memory = NULL;
+    data->dirty = 0;
+    return data;
 }
 
 
@@ -102,57 +104,55 @@ NITFPRIV(char*) getText(xmlNode* node)
  */
 NITFPRIV(NITF_BOOL) putElementsInTRE(xmlNode* node, nitf_TRE* tre, const char* prepend, nitf_Error* error)
 {
-	xmlNode * current = NULL;
-
-	int depth = 0;
-	char lastName[512] = "";
-	for (current = node; current; current = current->next)
+    xmlNode * current = NULL;
+    
+    int depth = 0;
+    char lastName[512] = "";
+    for (current = node; current; current = current->next)
+    {
+	
+	if (current->type == XML_ELEMENT_NODE)
 	{
+	    
+	    char name[512];
+	    nitf_Field* field;
+	    char* text;
+	    
+	    if (strcmp((char*)current->name, lastName) == 0)
+	    {
+		depth++;
+	    }
+	    else depth = 0;
+	    
+	    strcpy(lastName, (char*)current->name);
+	    sprintf(name, "%s/%s[%d]", prepend, (char*)current->name, depth);
+	    
+	    putElementsInTRE(current->children, tre, name, error);
+	    
+	    text = getText(current->children);
+	    
+	    
+	    
+	    if (text != NULL)
+	    {
 		
-		if (current->type == XML_ELEMENT_NODE)
+		
+		field = nitf_Field_construct(strlen(text), NITF_BCS_A, error);
+		nitf_Field_setString(field, text, error);
+		//printf("Name: <%s>: %s\n", name, text);			
+		if (!nitf_HashTable_insert(tre->hash, name, field, error))
 		{
-			
-			char name[512];
-			nitf_Field* field;
-			char* text;
-			
-			printf("DBG: %s\n", (char*)current->name);
-			if (strcmp((char*)current->name, lastName) == 0)
-			{
-				depth++;
-			}
-			else depth = 0;
-
-			strcpy(lastName, (char*)current->name);
-			//printf("DBG: %s/%s[%d]", prepend, (char*)current->name, depth);
-			sprintf(name, "%s/%s[%d]", prepend, (char*)current->name, depth);
-
-			putElementsInTRE(current->children, tre, name, error);
-			
-			text = getText(current->children);
-			
-			 
-			
-			if (text != NULL)
-			{
-
-				
-				field = nitf_Field_construct(strlen(text), NITF_BCS_A, error);
-				nitf_Field_setString(field, text, error);
-				//printf("Name: <%s>: %s\n", name, text);			
-				if (!nitf_HashTable_insert(tre->hash, name, field, error))
-				{
-					//free(text);
-					return NITF_FAILURE;
-				}
-				//free(text);
-			}
-
-			
+		    //free(text);
+		    return NITF_FAILURE;
 		}
-		
+		//free(text);
+	    }
+	    
+	    
 	}
-	return NITF_SUCCESS;
+	
+    }
+    return NITF_SUCCESS;
 }
 
 
@@ -163,123 +163,176 @@ NITFPRIV(NITF_BOOL) putElementsInTRE(xmlNode* node, nitf_TRE* tre, const char* p
  *
  */
 NITFPRIV(NITF_BOOL) XMLTRE_read(nitf_IOHandle ioHandle, 
-							  nitf_TRE* tre, struct _nitf_Record* record, nitf_Error* error)
+				nitf_TRE* tre, 
+				struct _nitf_Record* record, nitf_Error* error)
 {
-	XMLTREData* treData = NULL;
-	
-	xmlNode* node;
-	
+    XMLTREData* treData = NULL;
+    
+    xmlNode* node;
+    
     //char *data = NULL; 
     NITF_BOOL success; 
     if (!tre) {goto CATCH_ERROR;} 
-	
-	treData = XMLTREData_construct(error);
-	if (!treData) goto CATCH_ERROR;
-
-	treData->dirty = 0;
-	treData->memory = (char*)malloc(tre->length); //xmlBufferCreateSize(tre->length);
-	treData->memorySize = tre->length;
+    
+    treData = XMLTREData_construct(error);
+    if (!treData) goto CATCH_ERROR;
+    
+    treData->dirty = 0;
+    treData->memory = (char*)malloc(tre->length); //xmlBufferCreateSize(tre->length);
+    treData->memorySize = tre->length;
     //data = (char*)NITF_MALLOC( tre->length ); 
-	if (!treData->memory) 
+    if (!treData->memory) 
     { 
         nitf_Error_init(error, NITF_STRERROR( NITF_ERRNO ),NITF_CTXT, NITF_ERR_MEMORY );
         goto CATCH_ERROR;
     }
-	memset(treData->memory, 0, tre->length);
-	success = nitf_TREUtils_readField(ioHandle, treData->memory, (int)tre->length, error);
+    memset(treData->memory, 0, tre->length);
+    success = nitf_TREUtils_readField(ioHandle, treData->memory, (int)tre->length, error);
     if (!success) goto CATCH_ERROR;
-	
-	
-
-	
-	//xmlBufferAddHead(treData->memory, (const xmlChar*)data, tre->length);
-
-	//treData->memorySize = tre->length;
-	treData->doc = xmlReadMemory(treData->memory, treData->memorySize, NULL, NULL, 0);
-
-	tre->priv = treData;
-	
-	node = xmlDocGetRootElement(treData->doc);
-	//putElementsInTRE(node, tre);
-
-	//  print_element_names(node);
-	putElementsInTRE(node, tre, "", error);
+    
+    treData->doc = xmlReadMemory(treData->memory, treData->memorySize, NULL, NULL, 0);
+    
+    tre->priv = treData;
+    
+    node = xmlDocGetRootElement(treData->doc);
+    //putElementsInTRE(node, tre);
+    
+    //  print_element_names(node);
+    putElementsInTRE(node, tre, "", error);
     /*free the document */
     //xmlFreeDoc(doc);
-	//tre->
+    //tre->
     /*
      *Free the global variables that may
      *have been allocated by the parser.
      */
     xmlCleanupParser();
-	return NITF_SUCCESS;
-CATCH_ERROR:
-	if (treData->memory) NITF_FREE(treData->memory);
-	if (treData) NITF_FREE(treData);
+    return NITF_SUCCESS;
+ CATCH_ERROR:
+    if (treData->memory) NITF_FREE(treData->memory);
+    if (treData) NITF_FREE(treData);
     return NITF_FAILURE;
-
+    
 }
 
 
 
 NITFPRIV(NITF_BOOL) XMLTRE_initData(nitf_TRE * tre, const char* id, nitf_Error * error)
 {
-	
-	/* first, we need to build our DOM */
-	LIBXML_TEST_VERSION
-	//xmlSetBufferAllocationScheme(XML_BUFFER_ALLOC_EXACT);
+    
+    /* first, we need to build our DOM */
+    LIBXML_TEST_VERSION
+    //xmlSetBufferAllocationScheme(XML_BUFFER_ALLOC_EXACT);
     return NITF_SUCCESS;
 }
 
 NITFPRIV(NITF_BOOL) XMLTREData_updateBuffer(XMLTREData* treData, nitf_Error* error)
 {
-	if (!treData->dirty && treData->memory)
-	{
-		/* We can just write it out */
-		return NITF_SUCCESS;
-	}
-	/* If we have the memory (and its just dirty) we need to free it */
-	else if (treData->memory)
-	{
-		//xmlBufferFree(treData->memory);
-		free(treData->memory);
-	}
-	xmlDocDumpMemory(treData->doc, (xmlChar**)& (treData->memory), & (treData->memorySize));
-
-	/* In either case, dirty or memory need to allocate a buffer and get it */
-	//treData->memory = xmlBufferCreate();
-
-	//xmlSaveFileTo(treData->memory, treData->doc, NULL); // UTF-8??? 
-	//xmlNode* node = xmlDocGetRootElement(treData->doc);
-	
-	treData->dirty = 0;
+    if (!treData->dirty && treData->memory)
+    {
+	/* We can just write it out */
 	return NITF_SUCCESS;
+    }
+    /* If we have the memory (and its just dirty) we need to free it */
+    else if (treData->memory)
+    {
+	//xmlBufferFree(treData->memory);
+	free(treData->memory);
+    }
+    xmlDocDumpMemory(treData->doc, (xmlChar**)& (treData->memory), & (treData->memorySize));
+    
+    /* In either case, dirty or memory need to allocate a buffer and get it */
+    //treData->memory = xmlBufferCreate();
+    
+    //xmlSaveFileTo(treData->memory, treData->doc, NULL); // UTF-8??? 
+    //xmlNode* node = xmlDocGetRootElement(treData->doc);
+    
+    treData->dirty = 0;
+    return NITF_SUCCESS;
 }
 
 NITFPRIV(NITF_BOOL) XMLTRE_write(nitf_IOHandle ioHandle, nitf_TRE* tre, 
-							   struct _nitf_Record* record, nitf_Error* error)
+				 struct _nitf_Record* record, nitf_Error* error)
 {
-	//const char* xmlData;
-	//int xmlDataSize;
-	XMLTREData * treData = (XMLTREData*)tre->priv;
-	
-	/* Write out our DOM here */	
-	if (!XMLTREData_updateBuffer(treData, error)) return NITF_FAILURE;
-	//xmlDataSize = xmlBufferLength(treData->memory);
-	//xmlData = (const char*)xmlBufferContent(treData->memory);
-	if (!nitf_IOHandle_write(ioHandle, treData->memory, treData->memorySize, error))
-		return NITF_FAILURE;
-
-	return NITF_SUCCESS;
+    //const char* xmlData;
+    //int xmlDataSize;
+    XMLTREData * treData = (XMLTREData*)tre->priv;
+    
+    /* Write out our DOM here */	
+    if (!XMLTREData_updateBuffer(treData, error)) return NITF_FAILURE;
+    //xmlDataSize = xmlBufferLength(treData->memory);
+    //xmlData = (const char*)xmlBufferContent(treData->memory);
+    if (!nitf_IOHandle_write(ioHandle, treData->memory, treData->memorySize, error))
+	return NITF_FAILURE;
+    
+    return NITF_SUCCESS;
 }
 
 NITFPRIV(int) XMLTREIterator_getCurrentSize(nitf_TRE* tre, nitf_Error* error)
 {
-	int xmlDataSize;
-	XMLTREData * treData = (XMLTREData*)tre->priv;
-	if (!XMLTREData_updateBuffer(treData, error)) return -1;
-	return treData->memorySize;
-	//return xmlBufferLength(treData->memory);
+    XMLTREData * treData = (XMLTREData*)tre->priv;
+    if (!XMLTREData_updateBuffer(treData, error)) return -1;
+    return treData->memorySize;
+    //return xmlBufferLength(treData->memory);
+}
+
+
+NITFPRIV(nitf_List*) getFieldsFromXPath(xmlNodeSet* nodes)
+{
+    int i, size;
+    xmlNode* current;
+    size = (nodes) ? nodes->nodeNr : 0;
+
+    for (i = 0; i < size; i++)
+    {
+	if (nodes->nodeTab[i]->type == XML_NAMESPACE_DECL)
+	{
+	    printf("XML Namespace Decl\n");
+	}
+	else if (nodes->nodeTab[i]->type == XML_ELEMENT_NODE)
+	{
+	    current = nodes->nodeTab[i];
+	    printf("Found node: %s\n", current->name);
+	}
+	else
+	{
+	    current = nodes->nodeTab[i];
+	}
+    }
+    return NULL;
+
+}
+
+NITFPRIV(nitf_List*) XMLTRE_find(nitf_TRE* tre, const char* pattern, nitf_Error* error)
+{
+    nitf_List* list;
+    xmlXPathContext* xpathContext;
+    xmlXPathObject* xpathObject;
+    /* We want XPath to get the job done here */
+    XMLTREData* treData = (XMLTREData*)tre->priv;
+    xpathContext = xmlXPathNewContext(treData->doc);
+    if (xpathContext == NULL)
+    {
+	/* How do we solve these errors */
+	return NULL;
+    }
+    xpathObject = xmlXPathEvalExpression((const xmlChar*)pattern, xpathContext);
+    if (xpathObject == NULL)
+    {
+	xmlXPathFreeContext(xpathContext);
+	return NULL;
+    }
+    
+    list = getFieldsFromXPath(xpathObject->nodesetval);
+    
+
+    xmlXPathFreeObject(xpathObject);
+    xmlXPathFreeContext(xpathContext);
+    
+    return list;
+    
+
+
 }
 
 
@@ -343,6 +396,8 @@ NITFPRIV(NITF_BOOL) XMLTRE_increment(nitf_TREEnumerator** it, nitf_Error* error)
 
 }
 
+
+
 NITFPRIV(nitf_Pair*) XMLTREIterator_get(nitf_TREEnumerator* it, nitf_Error* error)
 {
 	nitf_HashTableIterator* hashIter = (nitf_HashTableIterator*)it->data;
@@ -376,6 +431,7 @@ static nitf_TREHandler gHandler =
 	XMLTRE_initData,
 	XMLTRE_read,
 	XMLTRE_setField,
+	XMLTRE_find,
 	XMLTRE_write,
 	XMLTRE_begin,
 	XMLTREIterator_getCurrentSize,
