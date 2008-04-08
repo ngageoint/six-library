a9fd2b5799710c5ef2774fe96079a8e791e2de8d
Tom Zellman <tzellman@github.com>
2008-04-08 13:10:27 +0000

    updated version and renamed src to source (for standardization)
    git-svn-id: http://svn.code.sf.net/p/nitro-nitf/code/trunk@141 7dc3ea37-dbd3-4c13-bb9a-7ccd57e75167

--- python/nitf/setup.py
+++ python/nitf/setup.py
@@ -3,9 +3,9 @@ from distutils.command.install import INSTALL_SCHEMES
 import os, glob, sys, shutil
 
 files = [os.path.normpath(f).replace('\\', '/') for f in \
-              glob.glob('src/main/python/*.dll') + \
-              glob.glob('src/main/python/nitf/*.so') + \
-              glob.glob('src/main/python/nitf/*.pyd')]
+              glob.glob('source/main/python/*.dll') + \
+              glob.glob('source/main/python/nitf/*.so') + \
+              glob.glob('source/main/python/nitf/*.pyd')]
 
 
 out_dir = 'build/lib/nitf'
@@ -18,13 +18,13 @@ for f in files:
 
 setup(
     name = 'nitro-python',
-    version = '1.5-rc6'
+    version = '1.5-rc7'
     url = 'http://nitro-nitf.sourceforge.net',
     author = 'tzellman',
     author_email = 'tzellman@users.sourceforget.net',
     description = 'Python bindings for the NITRO project',
     packages = ['nitf'],
-    package_dir = {'':'src/main/python'},
+    package_dir = {'':'source/main/python'},
     license = "LGPL",
-    scripts=['src/test/python/nitf_extract.py', 'src/test/python/nitf_print.py'],
+    scripts=['source/test/python/nitf_extract.py', 'source/test/python/nitf_print.py'],
 )
