879c7168924b5435c14b193a201c2a00f037aa44
gojira_1 <gojira_1@github.com>
2009-05-11 17:22:38 +0000

    Added geo utils
    git-svn-id: http://svn.code.sf.net/p/nitro-nitf/code/trunk@504 7dc3ea37-dbd3-4c13-bb9a-7ccd57e75167

--- c/nitf/source/Utils.c
+++ c/nitf/source/Utils.c
@@ -201,3 +201,223 @@ NITFPROT(void) nitf_Utils_baseName(char *base,
     memcpy(base, &fullName[begin], end - begin + 1);
     base[ end - begin + 1] = 0;
 }
+
+NITFAPI(void) nitf_Utils_decimalToGeographic(double decimal,
+                                             int *degrees,
+                                             int *minutes,
+                                             int *seconds)
+{
+    double remainder;
+    *degrees = (int)decimal;
+    remainder = fabs( decimal - (double)*degrees ) * 60.0;
+    *minutes = (int)remainder;
+    *seconds = fabs(remainder - (double)*minutes) * 60.0;
+
+}
+
+NITFAPI(double) nitf_Utils_geographicToDecimal(int degrees,
+                                               int minutes,
+                                               int seconds)
+{
+    double decimal = degrees;
+    decimal += ((double)minutes/60.0);
+    decimal += ((double)seconds/3600.0);
+
+    if (degrees < 0)
+    {
+        decimal *= -1;
+    }
+
+    return decimal;
+}
+
+NITFAPI(NITF_BOOL) nitf_Utils_parseGeographicString(char* dms,
+                                                    int* degrees,
+                                                    int* minutes,
+                                                    int* seconds,
+                                                    nitf_Error* error)
+{
+    int degreeOffset = 0;
+    int len = strlen(dms);
+    char dir;
+
+    char d[4];
+    char m[3];
+    char s[3];
+
+    /* ddmmssX or dddmmssY */
+    if (len == 7)
+    {
+        degreeOffset = 2;
+    }
+    else if (len == 8)
+    {
+        degreeOffset = 3;
+    }
+    else
+    {
+        nitf_Error_initf(error, NITF_CTXT, NITF_ERR_INVALID_PARAMETER,
+                         "Invalid decimal string: %s. Should be ddmmssX or dddmmssY",
+                         dms);
+        return NITF_FAILURE;
+    }
+    dir = dms[len - 1];
+    if (dir != 'N' && dir != 'S' && dir != 'E' && dir != 'W')
+    {
+        nitf_Error_initf(error, NITF_CTXT, NITF_ERR_INVALID_PARAMETER,
+                         "Invalid direction: %s. Should be [NSEW]",
+                         dms);
+        return NITF_FAILURE;
+    }
+
+    /* Now replace all spaces */
+    nitf_Utils_replace(dms, ' ', '0');
+
+
+    /* Now get the corners out as geographic coords */    
+    d[degreeOffset] = 0;
+    memcpy(d, dms, degreeOffset);
+
+    memcpy(m, &dms[degreeOffset], 2);
+    m[2] = 0;
+
+    memcpy(s, &dms[degreeOffset+2], 2);
+    s[2] = 0;
+    
+    *degrees = NITF_ATO32(d);
+    *minutes = NITF_ATO32(m);
+    *seconds = NITF_ATO32(s);
+
+    if ((degreeOffset == 2 && dir == 'S') ||
+        (degreeOffset == 3 && dir == 'W'))
+    {
+        *degrees *= -1;
+    }
+    
+    return NITF_SUCCESS;
+}
+
+NITFAPI(NITF_BOOL) nitf_Utils_parseDecimalString(char* d,
+                                                 double *decimal,
+                                                 nitf_Error* error)
+{
+    /* +-dd.ddd or += ddd.ddd */
+    int degreeOffset = 0;
+    int len = strlen(d);
+    char sign = d[0];
+    if (len == 7)
+    {
+        degreeOffset = 2;
+    }
+    else if (len == 8)
+    {
+        degreeOffset = 3;
+    }
+    else
+    {
+        nitf_Error_initf(error, NITF_CTXT, NITF_ERR_INVALID_PARAMETER,
+                         "Invalid decimal string: %s. Should be +-dd.ddd or +-ddd.ddd",
+                         d);
+        return NITF_FAILURE;
+    }
+    /* Now replace all spaces */
+    nitf_Utils_replace(d, ' ', '0');
+    *decimal = atof(&(d[1]));
+
+    if (sign == '-')
+    {
+        *decimal *= -1;
+    }
+
+    return NITF_SUCCESS;
+}
+
+NITFAPI(char) nitf_Utils_cornersTypeAsCoordRep(nitf_CornersType type)
+{
+    char cornerRep = ' ';
+
+    switch (type)
+    {
+    case NITF_CORNERS_UTM:
+        cornerRep = 'U';
+        break;
+
+    case NITF_CORNERS_UTM_UPS_S:
+        cornerRep = 'S';
+        break;
+
+    case NITF_CORNERS_UTM_UPS_N:
+        cornerRep = 'N';
+        break;
+
+    case NITF_CORNERS_GEO:
+        cornerRep = 'G';
+        break;
+
+    case NITF_CORNERS_DECIMAL:
+        cornerRep = 'G';
+        break;
+    default:
+        break;
+    }
+    return cornerRep;
+}
+
+NITFPROT(void) nitf_Utils_geographicLatToCharArray(int degrees,
+                                                   int minutes,
+                                                   int seconds,
+                                                   char *buffer7)
+{
+    char dir = 'N';
+    if (degrees < 0)
+    {
+        dir = 'S';
+        degrees *= -1;
+    }
+    sprintf(buffer7, "%02d%02d%02d%c", degrees, minutes, seconds, dir);
+}
+
+NITFPROT(void) nitf_Utils_geographicLonToCharArray(int degrees,
+                                                   int minutes,
+                                                   int seconds, 
+                                                   char *buffer8)
+{
+    char dir = 'E';
+    if (degrees < 0)
+    {
+        dir = 'E';
+    }
+    sprintf(buffer8, "%03d%02d%02d%c", degrees, minutes, seconds, dir);
+
+}
+
+NITFPROT(void) nitf_Utils_decimalLatToCharArray(double decimal,
+                                                char *buffer7)
+{
+    sprintf(buffer7, "%+-2.3f", decimal);
+}
+
+NITFPROT(void) nitf_Utils_decimalLonToCharArray(double decimal,
+                                               char *buffer8)
+{
+    sprintf(buffer8, "%+-3.3f", decimal);
+}
+
+NITFPROT(void) nitf_Utils_decimalLatToGeoCharArray(double decimal,
+                                                   char *buffer7)
+{
+    int d, m, s;
+    
+    nitf_Utils_decimalToGeographic(decimal, &d, &m, &s);
+    nitf_Utils_geographicLatToCharArray(d, m, s, buffer7);
+}
+
+NITFPROT(void) nitf_Utils_decimalLonToGeoCharArray(double decimal,
+                                                  char *buffer8)
+{
+
+    int d, m, s;
+    
+    nitf_Utils_decimalToGeographic(decimal, &d, &m, &s);
+    nitf_Utils_geographicLonToCharArray(d, m, s, buffer8);
+}
