baf44e5c58f59871f3b45825dc228cfd6de777fd
Tom Zellman <tzellman@github.com>
2009-09-23 19:36:52 +0000

    fixed DateTime - now uses a timegm equivalent
    git-svn-id: http://svn.code.sf.net/p/nitro-nitf/code/trunk@609 7dc3ea37-dbd3-4c13-bb9a-7ccd57e75167

--- c/nitf/source/DateTime.c
+++ c/nitf/source/DateTime.c
@@ -63,6 +63,35 @@ NITFAPI(nitf_DateTime*) nitf_DateTime_fromMillis(double millis,
     return dt;
 }
 
+NITFPRIV(time_t) nitf_DateTime_timegm(struct tm *t)
+{
+     time_t tl, tb;
+     struct tm *tg;
+
+    tl = mktime (t);
+    if (tl == -1)
+    {
+        t->tm_hour--;
+        tl = mktime (t);
+        if (tl == -1)
+            return -1; /* can't deal with output from strptime */
+        tl += 3600;
+    }
+    tg = gmtime (&tl);
+    tg->tm_isdst = 0;
+    tb = mktime (tg);
+    if (tb == -1)
+    {
+        tg->tm_hour--;
+        tb = mktime (tg);
+        if (tb == -1)
+            return -1; /* can't deal with output from gmtime */
+        tb += 3600;
+    }
+    return (tl - (tb - tl));
+}
+
+
 
 NITFAPI(nitf_DateTime*) nitf_DateTime_fromString(const char* string,
         const char* format, nitf_Error *error)
@@ -80,7 +109,7 @@ NITFAPI(nitf_DateTime*) nitf_DateTime_fromString(const char* string,
                 format);
         return NULL;
     }
-    return nitf_DateTime_fromMillis((double)time(&t) * 1000, error);
+    return nitf_DateTime_fromMillis((double)nitf_DateTime_timegm(&t) * 1000, error);
 #endif
 }
 
