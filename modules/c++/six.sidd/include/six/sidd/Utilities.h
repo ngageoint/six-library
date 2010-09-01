b054fbf6380a1cc0863e074e0e7c4d5c0db49713
Tom Zellman <tom.zellman@gd-ais.com>
2010-09-01 20:56:59 +0000

    updated sidd utilities
    git-svn-id: https://wush.net/svn/mdaus-oss/trunk@202 9d772353-a6c9-4ee9-b2cc-df47a7649cd5

--- modules/c++/six.sidd/include/six/sidd/Utilities.h
+++ modules/c++/six.sidd/include/six/sidd/Utilities.h
@@ -4,7 +4,6 @@
 #include <import/scene.h>
 #include "six/sidd/DerivedData.h"
 
-
 namespace six
 {
 namespace sidd
@@ -12,41 +11,28 @@ namespace sidd
 class Utilities
 {
 public:
-    
+
     static scene::SceneGeometry*
-        getSceneGeometry(DerivedData* derived);
-    
+    getSceneGeometry(DerivedData* derived);
 
-    static void setProductValues(Poly2D timeCOAPoly,
-                            PolyXYZ arpPoly, 
-                            ReferencePoint ref, 
-                            Vector3* row,
-                            Vector3* col, 
-                            RangeAzimuth<double> res,
-                            Product* product);
+    static void setProductValues(Poly2D timeCOAPoly, PolyXYZ arpPoly,
+            ReferencePoint ref, Vector3* row, Vector3* col,
+            RangeAzimuth<double>res, Product* product);
 
-    static void setProductValues(Vector3 arpVel, 
-                                 Vector3 arpPos,
-                                 Vector3 refPos, 
-                                 Vector3* row, 
-                                 Vector3* col,
-                                 RangeAzimuth<double> res, 
-                                 Product* product);
+    static void setProductValues(Vector3 arpVel, Vector3 arpPos,
+            Vector3 refPos, Vector3* row, Vector3* col,
+            RangeAzimuth<double>res, Product* product);
 
-    static void setCollectionValues(Poly2D timeCOAPoly,
-                                    PolyXYZ arpPoly, 
-                                    ReferencePoint ref, 
-                                    Vector3* row,
-                                    Vector3* col,
-                                    Collection* collection);
-    
-    static void setCollectionValues(Vector3 arpVel,
-                                    Vector3 arpPos, 
-                                    Vector3 refPos, 
-                                    Vector3* row,
-                                    Vector3* col, 
-                                    Collection* collection);
-    
+    static void setCollectionValues(Poly2D timeCOAPoly, PolyXYZ arpPoly,
+            ReferencePoint ref, Vector3* row, Vector3* col,
+            Collection* collection);
+
+    static void setCollectionValues(Vector3 arpVel, Vector3 arpPos,
+            Vector3 refPos, Vector3* row, Vector3* col, Collection* collection);
+
+    static std::pair<six::PolarizationType, six::PolarizationType>
+            convertDualPolarization(six::DualPolarizationType pol);
+
 };
 }
 }
--- modules/c++/six.sidd/source/Utilities.cpp
+++ modules/c++/six.sidd/source/Utilities.cpp
@@ -5,45 +5,93 @@ using namespace six;
 scene::SceneGeometry*
 six::sidd::Utilities::getSceneGeometry(DerivedData* derived)
 {
+    double centerTime;
+    if (derived->measurement->projection->isMeasurable())
+    {
+        six::sidd::MeasurableProjection
+                *projection =
+                        (six::sidd::MeasurableProjection*) derived->measurement->projection;
+        centerTime
+                = projection->timeCOAPoly(
+                                          projection->referencePoint.rowCol.row,
+                                          projection->referencePoint.rowCol.col);
+    }
+    else
+    {
+        // we estimate...
+        centerTime
+                = derived->exploitationFeatures->collections[0]->information->collectionDuration
+                        / 2;
+    }
 
-    if (! derived->measurement->projection->isMeasurable() )
-	throw except::Exception(Ctxt("Projection is not measurable"));
+    // compute arpPos and arpVel
+    six::Vector3 arpPos = derived->measurement->arpPoly(centerTime);
+    six::Vector3 arpVel =
+            derived->measurement->arpPoly.derivative()(centerTime);
+    six::Vector3 refPt = derived->measurement->projection->referencePoint.ecef;
 
-    MeasurableProjection* measurableProjection =
-	(MeasurableProjection*)derived->measurement->projection;
+    six::Vector3 *rowVec = NULL;
+    six::Vector3 *colVec = NULL;
 
-    Poly2D timeCOAPoly = measurableProjection->timeCOAPoly;
-    ReferencePoint ref = measurableProjection->referencePoint;
-    PolyXYZ arpPoly = derived->measurement->arpPoly;
+    if (derived->measurement->projection->projectionType
+            == six::ProjectionType::POLYNOMIAL)
+    {
+        six::sidd::PolynomialProjection
+                *projection =
+                        (six::sidd::PolynomialProjection*) derived->measurement->projection;
 
-    Vector3 refPos = ref.ecef;
-    double scpTime = timeCOAPoly(ref.rowCol.row, ref.rowCol.col);
+        double cR = projection->referencePoint.rowCol.row;
+        double cC = projection->referencePoint.rowCol.col;
 
-    Vector3 arpPos = arpPoly(scpTime);
-    PolyXYZ arpVelPoly = arpPoly.derivative();
-    Vector3 arpVel = arpVelPoly(scpTime);
+        scene::LatLonAlt centerLLA;
+        centerLLA.setLat(projection->rowColToLat(cR, cC));
+        centerLLA.setLon(projection->rowColToLon(cR, cC));
+        six::Vector3 centerEcef = scene::Utilities::latLonToECEF(centerLLA);
 
-    scene::SceneGeometry* geom = new scene::SceneGeometry(arpVel, arpPos,
-            refPos);
+        scene::LatLonAlt downLLA;
+        downLLA.setLat(projection->rowColToLat(cR + 1, cC));
+        downLLA.setLon(projection->rowColToLon(cR + 1, cC));
+        six::Vector3 downEcef = scene::Utilities::latLonToECEF(downLLA);
 
-    PlaneProjection* proj =
-            (PlaneProjection*) derived->measurement->projection;
+        scene::LatLonAlt rightLLA;
+        rightLLA.setLat(projection->rowColToLat(cR, cC + 1));
+        rightLLA.setLon(projection->rowColToLon(cR, cC + 1));
+        six::Vector3 rightEcef = scene::Utilities::latLonToECEF(rightLLA);
 
-    // These vectors are junk as soon as derived object goes away
-    geom->setImageVectors(&(proj->productPlane.rowUnitVector),
-            &(proj->productPlane.colUnitVector));
+        rowVec = new six::Vector3(downEcef - centerEcef);
+        rowVec->normalize();
+        colVec = new six::Vector3(rightEcef - centerEcef);
+        colVec->normalize();
+    }
+    else if (derived->measurement->projection->projectionType
+            == six::ProjectionType::PLANE)
+    {
+        six::sidd::PlaneProjection *projection =
+                (six::sidd::PlaneProjection*) derived->measurement->projection;
 
-    return geom;
+        rowVec = new six::Vector3(projection->productPlane.rowUnitVector);
+        colVec = new six::Vector3(projection->productPlane.colUnitVector);
+    }
+    else
+    {
+        throw except::Exception(
+                                Ctxt(
+                                     "Geographic and Cylindrical projectsions not yet supported"));
+    }
+
+    //    std::cout << "Center Time: " << centerTime << std::endl;
+    //    std::cout << "ARP Center Pos: " << arpPos << std::endl;
+    //    std::cout << "ARP Center Vel: " << arpVel << std::endl;
+    //    std::cout << "Scene Center Pos: " << refPt << std::endl;
+    //    std::cout << "Row Vec: " << rowVec << std::endl;
+    //    std::cout << "Col Vec: " << colVec << std::endl;
+
+    return new scene::SceneGeometry(arpVel, arpPos, refPt, rowVec, colVec, true);
 }
 
-
 void six::sidd::Utilities::setProductValues(Poly2D timeCOAPoly,
-                                 PolyXYZ arpPoly,
-                                 ReferencePoint ref,
-                                 Vector3* row,
-                                 Vector3* col,
-                                 RangeAzimuth<double> res,
-                                 Product* product)
+        PolyXYZ arpPoly, ReferencePoint ref, Vector3* row, Vector3* col,
+        RangeAzimuth<double>res, Product* product)
 {
     double scpTime = timeCOAPoly(ref.rowCol.row, ref.rowCol.col);
 
@@ -54,16 +102,12 @@ void six::sidd::Utilities::setProductValues(Poly2D timeCOAPoly,
     setProductValues(arpVel, arpPos, ref.ecef, row, col, res, product);
 }
 
-void six::sidd::Utilities::setProductValues(Vector3 arpVel,
-                                 Vector3 arpPos,
-                                 Vector3 refPos,
-                                 Vector3* row,
-                                 Vector3* col,
-                                 RangeAzimuth<double> res,
-                                 Product* product)
+void six::sidd::Utilities::setProductValues(Vector3 arpVel, Vector3 arpPos,
+        Vector3 refPos, Vector3* row, Vector3* col, RangeAzimuth<double>res,
+        Product* product)
 {
     scene::SceneGeometry* sceneGeom = new scene::SceneGeometry(arpVel, arpPos,
-            refPos);
+                                                               refPos);
     sceneGeom->setImageVectors(row, col);
 
     //do some setup of derived data from geometry
@@ -76,18 +120,16 @@ void six::sidd::Utilities::setProductValues(Vector3 arpVel,
     //    == Init::undefined<RowColDouble>())
     {
         sceneGeom->getGroundResolution(res.range, res.azimuth,
-                product->resolution.row, product->resolution.col);
+                                       product->resolution.row,
+                                       product->resolution.col);
     }
 
     delete sceneGeom;
 }
 
 void six::sidd::Utilities::setCollectionValues(Poly2D timeCOAPoly,
-                                    PolyXYZ arpPoly,
-                                    ReferencePoint ref,
-                                    Vector3* row,
-                                    Vector3* col,
-                                    Collection* collection)
+        PolyXYZ arpPoly, ReferencePoint ref, Vector3* row, Vector3* col,
+        Collection* collection)
 {
     double scpTime = timeCOAPoly(ref.rowCol.row, ref.rowCol.col);
 
@@ -98,12 +140,11 @@ void six::sidd::Utilities::setCollectionValues(Poly2D timeCOAPoly,
     setCollectionValues(arpVel, arpPos, ref.ecef, row, col, collection);
 }
 
-void six::sidd::Utilities::setCollectionValues(Vector3 arpVel,
-        Vector3 arpPos, Vector3 refPos, Vector3* row,
-        Vector3* col, Collection* collection)
+void six::sidd::Utilities::setCollectionValues(Vector3 arpVel, Vector3 arpPos,
+        Vector3 refPos, Vector3* row, Vector3* col, Collection* collection)
 {
     scene::SceneGeometry* sceneGeom = new scene::SceneGeometry(arpVel, arpPos,
-            refPos);
+                                                               refPos);
     sceneGeom->setImageVectors(row, col);
 
     if (collection->geometry == NULL)
@@ -141,21 +182,58 @@ void six::sidd::Utilities::setCollectionValues(Vector3 arpVel,
     }
     if (collection->phenomenology->groundTrack == Init::undefined<double>())
     {
-        collection->phenomenology->groundTrack = sceneGeom->getImageAngle(
-                sceneGeom->getGroundTrack());
+        collection->phenomenology->groundTrack
+                = sceneGeom->getImageAngle(sceneGeom->getGroundTrack());
     }
 
-    if (collection->phenomenology->shadow == Init::undefined<
-            AngleMagnitude>())
+    if (collection->phenomenology->shadow == Init::undefined<AngleMagnitude>())
     {
         collection->phenomenology->shadow = sceneGeom->getShadow();
     }
 
-    if (collection->phenomenology->layover == Init::undefined<
-            AngleMagnitude>())
+    if (collection->phenomenology->layover == Init::undefined<AngleMagnitude>())
     {
         collection->phenomenology->layover = sceneGeom->getLayover();
     }
 
     delete sceneGeom;
+}
+
+six::PolarizationType _convertDualPolarization(six::DualPolarizationType pol,
+        bool useFirst)
+{
+    switch (pol)
+    {
+    case six::DualPolarizationType::OTHER:
+        return six::PolarizationType::OTHER;
+    case six::DualPolarizationType::V_V:
+        return six::PolarizationType::V;
+    case six::DualPolarizationType::V_H:
+        return useFirst ? six::PolarizationType::V : six::PolarizationType::H;
+    case six::DualPolarizationType::H_V:
+        return useFirst ? six::PolarizationType::H : six::PolarizationType::V;
+    case six::DualPolarizationType::H_H:
+        return six::PolarizationType::H;
+    case six::DualPolarizationType::RHC_RHC:
+        return six::PolarizationType::RHC;
+    case six::DualPolarizationType::RHC_LHC:
+        return useFirst ? six::PolarizationType::RHC
+                        : six::PolarizationType::LHC;
+    case six::DualPolarizationType::LHC_RHC:
+        return useFirst ? six::PolarizationType::LHC
+                        : six::PolarizationType::RHC;
+    case six::DualPolarizationType::LHC_LHC:
+        return six::PolarizationType::LHC;
+    default:
+        return six::PolarizationType::NOT_SET;
+    }
+}
+
+std::pair<six::PolarizationType, six::PolarizationType> six::sidd::Utilities::convertDualPolarization(
+        six::DualPolarizationType pol)
+{
+    std::pair<six::PolarizationType, six::PolarizationType> pols;
+    pols.first = _convertDualPolarization(pol, true);
+    pols.second = _convertDualPolarization(pol, false);
+    return pols;
 }
