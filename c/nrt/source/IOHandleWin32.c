c0b210a63599568e03ba3b83a1c2cc3cbc2bc229
Micah Bojrab <micah.bojrab@mdaus.com>
2012-11-05 17:33:02 +0000

    update because corrupted NITF were causing infinite loops because of unexpected EOF. This now errors properly
    git-svn-id: http://svn.code.sf.net/p/nitro-nitf/code/trunk@1123 7dc3ea37-dbd3-4c13-bb9a-7ccd57e75167

--- c/nrt/source/IOHandleWin32.c
+++ c/nrt/source/IOHandleWin32.c
@@ -79,6 +79,15 @@ NRTAPI(NRT_BOOL) nrt_IOHandle_read(nrt_IOHandle handle, char *buf, size_t size,
                            NRT_ERR_READING_FROM_FILE);
             return NRT_FAILURE;
         }
+        else if (bytesThisRead == 0)
+        {
+            // CreateFile does not fail when finding the EOF --
+            // instead it reports 0 bytes read, so this stops a inifinite loop
+            // from Unexpected EOF
+            nrt_Error_init(error, "Unexpected end of file", NRT_CTXT,
+                           NRT_ERR_READING_FROM_FILE);
+            return NRT_FAILURE;
+        }
 
         bytesRead += bytesThisRead;
         bytesRemaining -= bytesThisRead;
