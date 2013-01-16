65977b3a1c905f4c4b10afa227808eb4c0ac5c04
Jeff Randolph <jeffrey.randolph@mdaus.com>
2013-01-16 22:06:24 +0000

    Moved openjpeg source to external area under j2k
    git-svn-id: http://svn.code.sf.net/p/nitro-nitf/code/trunk@1142 7dc3ea37-dbd3-4c13-bb9a-7ccd57e75167

--- external/drivers/jasper-1.900.1/src/libjasper/include/jasper/jas_config.h
+++ external/drivers/jasper-1.900.1/src/libjasper/include/jasper/jas_config.h
@@ -2,28 +2,16 @@
 #define __CONFIG_H__
 
 
+#ifndef JAS_VERSION
+#define JAS_VERSION "1.900.1"
+#endif
+
 #ifndef longlong
 #define longlong long long
 #endif
 
-#ifndef PACKAGE
-#define PACKAGE "jasper"
-#endif
-
-#ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION "1.900.1"
-#endif
-
 #ifndef VERSION
 #define VERSION "1.900.1"
-#endif
-
-#ifndef PACKAGE_STRING
-#define PACKAGE_STRING "jasper 1.900.1"
-#endif
-
-#ifndef JAS_VERSION
-#define JAS_VERSION "1.900.1"
 #endif
 
 #ifndef ulonglong
@@ -34,6 +22,10 @@
 #define PACKAGE_NAME "jasper"
 #endif
 
+#ifndef PACKAGE
+#define PACKAGE "jasper"
+#endif
+
 #ifndef uchar
 #define uchar unsigned char
 #endif
@@ -42,8 +34,12 @@
 #define PACKAGE_TARNAME "jasper"
 #endif
 
-#ifndef ushort
-#define ushort unsigned short
+#ifndef PACKAGE_VERSION
+#define PACKAGE_VERSION "1.900.1"
+#endif
+
+#ifndef PACKAGE_STRING
+#define PACKAGE_STRING "jasper 1.900.1"
 #endif
 
 #endif /* __CONFIG_H__ */
