9aaa1d77da26e4f020df4d3f3b2e52631d1fe1cd
Adam Sylvester <adam.sylvester@mdaus.com>
2014-04-09 14:25:45 +0000

    Missed checkin
    git-svn-id: https://wush.net/svn/mdaus-oss/trunk@514 9d772353-a6c9-4ee9-b2cc-df47a7649cd5

---
+++ modules/c++/scene/include/scene/ProjectionPolynomialFitter.h
@@ -0,0 +1,201 @@
+/* =========================================================================
+ * This file is part of scene-c++
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2013, General Dynamics - Advanced Information Systems
+ *
+ * scene-c++ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef __SCENE_PROJECTION_POLYNOMIAL_FITTER_H__
+#define __SCENE_PROJECTION_POLYNOMIAL_FITTER_H__
+
+#include <scene/GridECEFTransform.h>
+#include <scene/ProjectionModel.h>
+#include <math/linear/Matrix2D.h>
+
+namespace scene
+{
+/*!
+ * \class ProjectionPolynomialFitter
+ * \brief Used to fit output --> slant and/or time COA polynomials based on
+ * sampling sceneToImage() across the output plane
+ * versa
+ */
+class ProjectionPolynomialFitter
+{
+public:
+    static const size_t DEFAULTS_POINTS_1D;
+
+
+    /* Samples a numPoints1D x numPoints1D grid of points that spans
+     * outExtent using sceneToImage().
+     *
+     * \param projModel Projection model that knows how to use sceneToImage()
+     * to convert from an ECEF location to meters from the slant plane ORP
+     * \param gridTransform Transform that knows how to convert from
+     * output row/col pixel space to ECEF space
+     * \param outExtent Output extent in pixels
+     * \param numPoints1D Number of points to use in each direction when
+     * sampling the grid.  Defaults to 10.
+     */
+    ProjectionPolynomialFitter(
+            const ProjectionModel& projModel,
+            const GridECEFTransform& gridTransform,
+            const types::RowCol<size_t>& outExtent,
+            size_t numPoints1D = DEFAULTS_POINTS_1D);
+
+    // Returns the output plane rows used during sampling in case you want to
+    // do your own polynomial fitting
+    const math::linear::Matrix2D<double>& getOutputPlaneRows() const
+    {
+        return mOutputPlaneRows;
+    }
+
+    // Returns the output plane cols used during sampling in case you want to
+    // do your own polynomial fitting
+    const math::linear::Matrix2D<double>& getOutputPlaneCols() const
+    {
+        return mOutputPlaneCols;
+    }
+
+    // Returns the scene coordinates (i.e. meters from the slant plane ORP)
+    // computed for each grid sample in case you want to do your own
+    // polynomial fitting
+    const math::linear::Matrix2D<types::RowCol<double> >&
+    getSceneCoordinates() const
+    {
+        return mSceneCoordinates;
+    }
+
+    // Returns the time center of aperture computed for each grid sample in
+    // case you want to do your own polynomial fitting
+    const math::linear::Matrix2D<double>& getTimeCOA() const
+    {
+        return mTimeCOA;
+    }
+
+    /*
+     * Uses the samples computed in the constructor to fit pixel-based
+     * output --> slant polynomials of the specified order.  Optionally
+     * computes mean residual errors in these polynomials (mean of the squares
+     * of the differences).
+     *
+     * interimSceneCenter and interimSampleSpacing are also in the slant
+     * plane.  These represent the image at the point when the output to slant
+     * polynomials will be applied.  They're intended to allow for any
+     * image upsampling, etc. that may be done while still in the slant plane.
+     * If no such upsampling is done, set interimSceneCenter = inSceneCenter
+     * and interimSampleSpacing = the original input slant plane sample
+     * spacing.
+     *
+     * \param inPixelStart Original input slant plane start pixel (i.e. if
+     * this is non-zero, it indicates an AOI)
+     * \param inSceneCenter Original input slant plane scene center pixel in
+     * row/col
+     * \param interimSceneCenter Scene center pixel in row/col of the image
+     * when outputToSlantRow/Col polynomials are applied
+     * \param interimSampleSpacing Sample spacing of the image when
+     * outputToSlantRow/Col polynomials are applied
+     * \param polyOrderX Polynomial order to use when fitting the polynomials
+     * in the x direction
+     * \param polyOrderY Polynomial order to use when fitting the polynomials
+     * in the y direction
+     * \param outputToSlantRow [output] Output to slant row polynomial.
+     * This is pixel-based.
+     * \param outputToSlantCol [output] Output to slant col polynomial.
+     * This is pixel-based.
+     * \param meanResidualErrorRow [output] Optional.  Mean residual error in
+     * outputToSlantRow.
+     * \param meanResidualErrorCol [output] Optional.  Mean residual error in
+     * outputToSlantCol.
+     */
+    void fitOutputToSlantPolynomials(
+            const types::RowCol<size_t>& inPixelStart,
+            const types::RowCol<double>& inSceneCenter,
+            const types::RowCol<double>& interimSceneCenter,
+            const types::RowCol<double>& interimSampleSpacing,
+            size_t polyOrderX,
+            size_t polyOrderY,
+            math::poly::TwoD<double>& outputToSlantRow,
+            math::poly::TwoD<double>& outputToSlantCol,
+            double* meanResidualErrorRow = NULL,
+            double* meanResidualErrorCol = NULL) const;
+
+    /*
+     * Uses the samples computed in the constructor to fit a time COA
+     * polynomial of the specified order in units of meters from
+     * outSceneCenter.  Optionally computes mean residual errors in these
+     * polynomials (mean of the squares of the differences).
+     *
+     * \param outSceneCenter Output space scene center pixel in row/col
+     * \param outSampleSpacing Output space sample spacing
+     * \param polyOrderX Polynomial order to use when fitting the polynomials
+     * in the x direction
+     * \param polyOrderY Polynomial order to use when fitting the polynomials
+     * in the y direction
+     * \param timeCOAPoly [output] Time center of aperture polynomial
+     * (in meters from outSceneCenter)
+     * \param meanResidualError [output] Optional.  Mean residual error in
+     * timeCOAPoly.
+     */
+    void fitTimeCOAPolynomial(
+            const types::RowCol<double>& outSceneCenter,
+            const types::RowCol<double>& outSampleSpacing,
+            size_t polyOrderX,
+            size_t polyOrderY,
+            math::poly::TwoD<double>& timeCOAPoly,
+            double* meanResidualError = NULL) const;
+
+    /*
+     * Uses the samples computed in the constructor to fit a time COA
+     * polynomial of the specified order in units of 0-based pixels from the
+     * upper-left corner.  Optionally computes mean residual errors in these
+     * polynomials (mean of the squares of the differences).
+     *
+     * \param outPixelStart Output space start pixel (i.e. if this is
+     * non-zero, it indicates the SCP used with gridTransform is with respect
+     * to a global space.  This basically translates to the segment's
+     * startLine/startSample values for a multi-segment SICD).
+     * This offset is subtracted from each pixel value used to fit the
+     * polynomial, so if you want a polynomial in units of 1-based pixels from
+     * the upper-left corner, you would set this to (-1, -1) plus whatever
+     * compensation is needed if it's a multi-segment SICD.
+     * \param polyOrderX Polynomial order to use when fitting the polynomials
+     * in the x direction
+     * \param polyOrderY Polynomial order to use when fitting the polynomials
+     * in the y direction
+     * \param timeCOAPoly [output] Time center of aperture polynomial
+     * (in pixels from the upper-left corner)
+     * \param meanResidualError [output] Optional.  Mean residual error in
+     * timeCOAPoly.
+     */
+    void fitPixelBasedTimeCOAPolynomial(
+            const types::RowCol<double>& outPixelStart,
+            size_t polyOrderX,
+            size_t polyOrderY,
+            math::poly::TwoD<double>& timeCOAPoly,
+            double* meanResidualError = NULL) const;
+
+private:
+    const size_t mNumPoints1D;
+    math::linear::Matrix2D<double> mOutputPlaneRows;
+    math::linear::Matrix2D<double> mOutputPlaneCols;
+    math::linear::Matrix2D<types::RowCol<double> > mSceneCoordinates;
+    math::linear::Matrix2D<double> mTimeCOA;
+};
+}
+
+#endif
---
+++ modules/c++/scene/source/ProjectionPolynomialFitter.cpp
@@ -0,0 +1,261 @@
+/* =========================================================================
+ * This file is part of scene-c++
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2013, General Dynamics - Advanced Information Systems
+ *
+ * scene-c++ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <math/poly/Fit.h>
+#include <scene/ProjectionPolynomialFitter.h>
+
+namespace scene
+{
+const size_t ProjectionPolynomialFitter::DEFAULTS_POINTS_1D = 10;
+
+ProjectionPolynomialFitter::ProjectionPolynomialFitter(
+        const ProjectionModel& projModel,
+        const GridECEFTransform& gridTransform,
+        const types::RowCol<size_t>& outExtent,
+        size_t numPoints1D) :
+    mNumPoints1D(numPoints1D),
+    mOutputPlaneRows(numPoints1D, numPoints1D),
+    mOutputPlaneCols(numPoints1D, numPoints1D),
+    mSceneCoordinates(numPoints1D,
+                      numPoints1D,
+                      types::RowCol<double>(0.0, 0.0)),
+    mTimeCOA(numPoints1D, numPoints1D)
+{
+    // Want to sample [0, outExtent) in the loop below
+    // That is, we are marching through the output grid in pixel space
+    const types::RowCol<double> skip(
+        static_cast<double>(outExtent.row - 1) / (mNumPoints1D - 1),
+        static_cast<double>(outExtent.col - 1) / (mNumPoints1D - 1));
+
+    types::RowCol<double> currentOffset(0.0, 0.0);
+
+    for (size_t ii = 0;
+         ii < mNumPoints1D;
+         ++ii, currentOffset.row += skip.row)
+    {
+        currentOffset.col = 0;
+
+        for (size_t jj = 0;
+             jj < mNumPoints1D;
+             ++jj, currentOffset.col += skip.col)
+        {
+            // Rows, these are essentially meshgrid-like (replicated)
+            mOutputPlaneRows(ii, jj) = currentOffset.row;
+            // This represents columns
+            mOutputPlaneCols(ii, jj) = currentOffset.col;
+
+            // Find ECEF location of this output plane pixel
+            const scene::Vector3 sPos =
+                    gridTransform.rowColToECEF(currentOffset);
+
+            // Call sceneToImage() to get meters from the ORP in the slant
+            // plane
+            double timeCOA(0.0);
+            mSceneCoordinates(ii, jj) =
+                    projModel.sceneToImage(sPos, &timeCOA);
+            mTimeCOA(ii, jj) = timeCOA;
+        }
+    }
+}
+
+void ProjectionPolynomialFitter::fitOutputToSlantPolynomials(
+        const types::RowCol<size_t>& inPixelStart,
+        const types::RowCol<double>& inSceneCenter,
+        const types::RowCol<double>& interimSceneCenter,
+        const types::RowCol<double>& interimSampleSpacing,
+        size_t polyOrderX,
+        size_t polyOrderY,
+        math::poly::TwoD<double>& outputToSlantRow,
+        math::poly::TwoD<double>& outputToSlantCol,
+        double* meanResidualErrorRow,
+        double* meanResidualErrorCol) const
+{
+    const types::RowCol<double> ratio(interimSceneCenter / inSceneCenter);
+
+    const types::RowCol<double> aoiOffset(inPixelStart.row * ratio.row,
+                                          inPixelStart.col * ratio.col);
+
+    math::linear::Matrix2D<double> slantPlaneRows(mNumPoints1D, mNumPoints1D);
+    math::linear::Matrix2D<double> slantPlaneCols(mNumPoints1D, mNumPoints1D);
+
+    for (size_t ii = 0; ii < mNumPoints1D; ++ii)
+    {
+        for (size_t jj = 0; jj < mNumPoints1D; ++jj)
+        {
+            // sceneCoord is in meters from the slant plane ORP
+            // So, divide by the sample spacing to get it in pixels, then
+            // offset by the slant plane ORP.  Need to further offset to take
+            // non-zero inPixelStart into account.
+            const types::RowCol<double> sceneCoord(mSceneCoordinates(ii, jj));
+
+            slantPlaneRows(ii, jj) =
+                    sceneCoord.row / interimSampleSpacing.row +
+                    interimSceneCenter.row - aoiOffset.row;
+
+            slantPlaneCols(ii, jj) =
+                    sceneCoord.col / interimSampleSpacing.col +
+                    interimSceneCenter.col - aoiOffset.col;
+        }
+    }
+
+    // Now fit the polynomials
+    outputToSlantRow = math::poly::fit(mOutputPlaneRows,
+                                       mOutputPlaneCols,
+                                       slantPlaneRows,
+                                       polyOrderX, polyOrderY);
+
+    outputToSlantCol = math::poly::fit(mOutputPlaneRows,
+                                       mOutputPlaneCols,
+                                       slantPlaneCols,
+                                       polyOrderX, polyOrderY);
+
+    // Optionally report the residual error
+    if (meanResidualErrorRow || meanResidualErrorCol)
+    {
+        double errorSumRow(0.0);
+        double errorSumCol(0.0);
+
+        for (size_t ii = 0; ii < mNumPoints1D; ++ii)
+        {
+            for (size_t jj = 0; jj < mNumPoints1D; ++jj)
+            {
+                const double row(mOutputPlaneRows(ii, jj));
+                const double col(mOutputPlaneCols(ii, jj));
+
+                double diff =
+                        slantPlaneRows(ii, jj) - outputToSlantRow(row, col);
+                errorSumRow += diff * diff;
+
+                diff = slantPlaneCols(ii, jj) - outputToSlantCol(row, col);
+                errorSumCol += diff * diff;
+            }
+        }
+
+        const size_t numPoints = mNumPoints1D * mNumPoints1D;
+        if (meanResidualErrorRow)
+        {
+            *meanResidualErrorRow = errorSumRow / numPoints;
+        }
+        if (meanResidualErrorCol)
+        {
+            *meanResidualErrorCol = errorSumCol / numPoints;
+        }
+    }
+}
+
+void ProjectionPolynomialFitter::fitTimeCOAPolynomial(
+        const types::RowCol<double>& outSceneCenter,
+        const types::RowCol<double>& outSampleSpacing,
+        size_t polyOrderX,
+        size_t polyOrderY,
+        math::poly::TwoD<double>& timeCOAPoly,
+        double* meanResidualError) const
+{
+    math::linear::Matrix2D<double> rowMapping(mNumPoints1D, mNumPoints1D);
+    math::linear::Matrix2D<double> colMapping(mNumPoints1D, mNumPoints1D);
+
+    for (size_t ii = 0; ii < mNumPoints1D; ++ii)
+    {
+        for (size_t jj = 0; jj < mNumPoints1D; ++jj)
+        {
+            // Need to map output plane pixels to meters from the output
+            // plane SCP
+            rowMapping(ii, jj) =
+                    (mOutputPlaneRows(ii,jj) - outSceneCenter.row) *
+                    outSampleSpacing.row;
+
+            colMapping(ii, jj) =
+                    (mOutputPlaneCols(ii,jj) - outSceneCenter.col) *
+                    outSampleSpacing.col;
+        }
+    }
+
+    // Now fit the polynomial
+    timeCOAPoly = math::poly::fit(rowMapping, colMapping, mTimeCOA,
+                                  polyOrderX, polyOrderY);
+
+    // Optionally report the residual error
+    if (meanResidualError)
+    {
+        double errorSum(0.0);
+
+        for (size_t ii = 0; ii < mNumPoints1D; ++ii)
+        {
+            for (size_t jj = 0; jj < mNumPoints1D; ++jj)
+            {
+                const double row(rowMapping(ii, jj));
+                const double col(colMapping(ii, jj));
+
+                const double diff = mTimeCOA(ii, jj) - timeCOAPoly(row, col);
+                errorSum += diff * diff;
+            }
+        }
+
+        *meanResidualError = errorSum / (mNumPoints1D * mNumPoints1D);
+    }
+}
+
+void ProjectionPolynomialFitter::fitPixelBasedTimeCOAPolynomial(
+        const types::RowCol<double>& outPixelStart,
+        size_t polyOrderX,
+        size_t polyOrderY,
+        math::poly::TwoD<double>& timeCOAPoly,
+        double* meanResidualError) const
+{
+    math::linear::Matrix2D<double> rowMapping(mNumPoints1D, mNumPoints1D);
+    math::linear::Matrix2D<double> colMapping(mNumPoints1D, mNumPoints1D);
+
+    for (size_t ii = 0; ii < mNumPoints1D; ++ii)
+    {
+        for (size_t jj = 0; jj < mNumPoints1D; ++jj)
+        {
+            // Allow the caller to offset the row/col that we fit to
+            rowMapping(ii, jj) = mOutputPlaneRows(ii,jj) - outPixelStart.row;
+            colMapping(ii, jj) = mOutputPlaneCols(ii,jj) - outPixelStart.col;
+        }
+    }
+
+    // Now fit the polynomial
+    timeCOAPoly = math::poly::fit(rowMapping, colMapping, mTimeCOA,
+                                  polyOrderX, polyOrderY);
+
+    // Optionally report the residual error
+    if (meanResidualError)
+    {
+        double errorSum(0.0);
+
+        for (size_t ii = 0; ii < mNumPoints1D; ++ii)
+        {
+            for (size_t jj = 0; jj < mNumPoints1D; ++jj)
+            {
+                const double row(rowMapping(ii, jj));
+                const double col(colMapping(ii, jj));
+
+                const double diff = mTimeCOA(ii, jj) - timeCOAPoly(row, col);
+                errorSum += diff * diff;
+            }
+        }
+
+        *meanResidualError = errorSum / (mNumPoints1D * mNumPoints1D);
+    }
+}
+}
