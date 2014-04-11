90ad9b80f4008b2f35fbc4274528385c7c159a9f
Adam Sylvester <adam.sylvester@mdaus.com>
2014-04-11 16:50:27 +0000

    It's useful to have an outPixelScaleFactor parameter too - added an overloading for this.  #343
    git-svn-id: https://wush.net/svn/mdaus-oss/trunk@517 9d772353-a6c9-4ee9-b2cc-df47a7649cd5

--- modules/c++/scene/include/scene/ProjectionPolynomialFitter.h
+++ modules/c++/scene/include/scene/ProjectionPolynomialFitter.h
@@ -113,6 +113,11 @@ public:
      * non-zero, it indicates the SCP used with gridTransform is with respect
      * to a global space.  This basically translates to the segment's
      * startLine/startSample values for a multi-segment SICD).
+     * \param outPixelScaleFactor Scale factor to multiply output plane pixel
+     * samples by (applied before outPixelStart is subtracted).  This is
+     * useful if your output plane sample spacing is actually different than
+     * what gridTransform contained (in this case, set outPixelScaleFactor =
+     * newSampleSpacing / origSampleSpacing)
      * \param polyOrderX Polynomial order to use when fitting the polynomials
      * in the x direction
      * \param polyOrderY Polynomial order to use when fitting the polynomials
@@ -126,6 +131,21 @@ public:
      * \param meanResidualErrorCol [output] Optional.  Mean residual error in
      * outputToSlantCol.
      */
+    void fitOutputToSlantPolynomials(
+            const types::RowCol<size_t>& inPixelStart,
+            const types::RowCol<double>& inSceneCenter,
+            const types::RowCol<double>& interimSceneCenter,
+            const types::RowCol<double>& interimSampleSpacing,
+            const types::RowCol<double>& outPixelStart,
+            const types::RowCol<double>& outPixelScaleFactor,
+            size_t polyOrderX,
+            size_t polyOrderY,
+            math::poly::TwoD<double>& outputToSlantRow,
+            math::poly::TwoD<double>& outputToSlantCol,
+            double* meanResidualErrorRow = NULL,
+            double* meanResidualErrorCol = NULL) const;
+
+    // Same as above but applies no output pixel scale factor
     void fitOutputToSlantPolynomials(
             const types::RowCol<size_t>& inPixelStart,
             const types::RowCol<double>& inSceneCenter,
