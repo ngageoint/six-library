106bf8f3a5e38c94095760354b4a7d3bcb5d1b4c
Adam Sylvester <adam.sylvester@mdaus.com>
2015-10-29 10:27:53 -0400

    Squashed 'externals/coda-oss/' changes from 37526a4..47c416f
    47c416f Merge pull request #59 from mdaus/math-round
    3288100 Fixed banners and added missing std namespaces
    f4f60fe Added math::round methods
    
    git-subtree-dir: externals/coda-oss
    git-subtree-split: 47c416f92b195938d2b089e77f6eaf74e0468ce2

--- modules/c++/math/include/math/Constants.h
+++ modules/c++/math/include/math/Constants.h
@@ -2,9 +2,9 @@
  * This file is part of math-c++
  * =========================================================================
  *
- * (C) Copyright 2004 - 2014, MDA Information Systems LLC
+ * (C) Copyright 2004 - 2015, MDA Information Systems LLC
  *
- * types-c++ is free software; you can redistribute it and/or modify
+ * math-c++ is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
