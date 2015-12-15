7e2f138a8d88ad47eaae4a5512e6df97c56ff8a9
Adam Sylvester <adam.sylvester@mdaus.com>
2015-12-15 14:01:42 -0500

    Adding Compression

---
+++ six/modules/c++/six.sidd/source/Compression.cpp
@@ -0,0 +1,23 @@
+#include <six/sidd/Compression.h>
+
+namespace six
+{
+namespace sidd
+{
+J2KCompression::Layer::Layer() :
+    bitRate(0.0)
+{
+}
+
+J2KCompression::J2KCompression() :
+    numWaveletLevels(0),
+    numBands(0)
+{
+}
+
+Compression::Compression() :
+    originalCompressedDataFlag(0)
+{
+}
+}
+}
