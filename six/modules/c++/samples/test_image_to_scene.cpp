bc4c4c0d34e4dcdb9f33b20c0c53484cd4921a1c
John Bruce <john.bruce@mdaus.com>
2016-04-13 11:36:30 -0400

    Update CODA logger api usage due to api change

--- six/modules/c++/samples/test_image_to_scene.cpp
+++ six/modules/c++/samples/test_image_to_scene.cpp
@@ -73,7 +73,7 @@ int main(int argc, char** argv)
         xmlRegistry->addCreator(six::DataType::DERIVED,
                             new six::XMLControlCreatorT<six::sidd::DerivedXMLControl>());
 
-        std::auto_ptr<logging::Logger> logger(logging::setupLogger(progname));
+        logging::LoggerPtr logger(logging::setupLogger(progname));
         six::NITFReadControl reader;
         reader.setLogger(logger.get());
         reader.setXMLControlRegistry(xmlRegistry);
