b14f22b40599e7c2a3cb76a562f9ac9d1ba057d1
JonathanMeans <jonathan.means@MDAUS.com>
2016-05-16 09:31:35 -0400

    Update testing scripts

--- six/modules/python/six.sicd/tests/test_create_sicd_xml.py
+++ six/modules/python/six.sicd/tests/test_create_sicd_xml.py
@@ -112,7 +112,7 @@ def initImageDataNITF(cmplx):
     return cmplx
 
 def initGeoData(cmplx):
-    geoData = makeScopedCloneableGeoData()
+    geoData = makeScopedCopyableGeoData()
     geoData.earthModel = EarthModelType('WGS84')
     geoData.scp.ecf[0] = 100
     geoData.scp.ecf[1] = 200
@@ -127,7 +127,7 @@ def initGeoData(cmplx):
     for i in range(4):
         geoData.validData.push_back(LatLon(i * 11, i * 11 + 13))
 
-    geoInfo = makeScopedCloneableGeoInfo()
+    geoInfo = makeScopedCopyableGeoInfo()
     geoInfo.name = 'My name'
     param = Parameter()
     param.setName('Some GeoData param')
