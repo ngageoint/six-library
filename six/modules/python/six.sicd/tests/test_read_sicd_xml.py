4f15b5cffe71b6841222f8efd99e7b2d27d6398a
Adam Sylvester <adam.sylvester@mdaus.com>
2016-05-26 18:37:39 -0400

    Adding script to show how to convert an XML file on disk or in a string to and from a ComplexData object

---
+++ six/modules/python/six.sicd/tests/test_read_sicd_xml.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+#
+# =========================================================================
+# This file is part of six.sicd-python
+# =========================================================================
+#
+# (C) Copyright 2004 - 2015, MDA Information Systems LLC
+#
+# six.sicd-python is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; If not,
+# see <http://www.gnu.org/licenses/>.
+#
+
+# Simple program to show how you can go from XML to and from SIX
+
+from pysix.six_sicd import *
+from pysix.six_base import *
+from coda.coda_logging import *
+import sys, os
+
+# Parse the command line
+if len(sys.argv) != 2:
+    sys.exit('Usage ' + sys.argv[0] + ' <XML pathname>')
+
+xmlPathname = sys.argv[1]
+
+schemaPaths = VectorString()
+schemaPaths.push_back(os.environ['SIX_SCHEMA_PATH'])
+logger = NullLogger()
+
+# Go from XML file on disk to ComplexData
+data = SixSicdUtilities.parseDataFromFile(xmlPathname, schemaPaths, logger)
+print data.getNumRows()
+
+# Go from ComplexData to string
+xmlStr = SixSicdUtilities.toXMLString(data, schemaPaths, logger)
+print xmlStr
+
+# Go from string back to ComplexData
+data2 = SixSicdUtilities.parseDataFromString(xmlStr, schemaPaths, logger)
+
+# Back to string one last time to compare
+xmlStr2 = SixSicdUtilities.toXMLString(data2, schemaPaths, logger)
+
+if xmlStr == xmlStr2:
+    print('Strings match')
+else:
+    sys.exit('Strings do not match!')
