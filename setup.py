aec3a91add4bb2c9553c2871c7faedd4df427553
JonathanMeans <jonathan.means@MDAUS.com>
2016-08-30 10:46:21 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, Distribution
+import glob
+import os
+import platform
+
+packages = ''
+if platform.system() == 'Windows':
+    packages = os.path.join('install', 'lib', 'site-packages')
+else:
+    packages = glob.glob(os.path.join(
+        'install', 'lib', 'python*', 'site-pacakges'))[0]
+
+
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
+
+codaPyds = glob.glob(os.path.join(packages, 'coda', '_*pyd'))
+codaPyds = [os.path.basename(pyd) for pyd in codaPyds]
+
+sixPyds = glob.glob(os.path.join(packages, 'pysix', '_*pyd'))
+sixPyds = [os.path.basename(pyd) for pyd in sixPyds]
+
+
+
+setup(name='pysix',
+      version='2.2.1',
+      package_dir = {'': packages},
+      description = str(codaPyds),
+      packages=['pysix', 'coda'],
+      package_data= {
+          'pysix': sixPyds,
+          'coda': codaPyds
+      },
+      distclass=BinaryDistribution)
