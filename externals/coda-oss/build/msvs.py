821cf99bceacc1f400bfc19b14b6ab44404e5bf8
JonathanMeans <jonathan.means@MDAUS.com>
2016-09-07 15:55:07 -0400

    Add more tabs

--- externals/coda-oss/build/msvs.py
+++ externals/coda-oss/build/msvs.py
@@ -354,7 +354,7 @@ except:
 def stealth_write(self, data, flags='wb'):
 	import sys
 	if sys.version_info[0] == 2:
-                data = data.decode(sys.getfilesystemencoding(), 'replace')
+		data = data.decode(sys.getfilesystemencoding(), 'replace')
 		data = data.encode('utf-8')
 	else:
 		data = data.encode('utf-8')
