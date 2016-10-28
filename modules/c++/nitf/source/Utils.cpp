3191a59673f04491a640646091c9fafb332851a0
Clyde Stanfield <cstanfie@MIINFCOMPUTE01.mdaus.corp>
2016-10-28 12:53:13 -0400

    Added banners

--- modules/c++/nitf/source/Utils.cpp
+++ modules/c++/nitf/source/Utils.cpp
@@ -1,3 +1,26 @@
+/* =========================================================================
+ * This file is part of NITRO
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2016, MDA Information Systems LLC
+ *
+ * NITRO is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #include "nitf/Utils.hpp"
 
 using namespace nitf;
@@ -27,3 +50,4 @@ char Utils::cornersTypeAsCoordRep(nitf::CornersType type)
 {
     return nitf_Utils_cornersTypeAsCoordRep(type);
 }
+
--- modules/c/nitf/include/nitf/TREDescription.h
+++ modules/c/nitf/include/nitf/TREDescription.h
@@ -1,3 +1,26 @@
+/* =========================================================================
+ * This file is part of NITRO
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2016, MDA Information Systems LLC
+ *
+ * NITRO is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #ifndef __NITF_TRE_DESCRIPTION_H__
 #define __NITF_TRE_DESCRIPTION_H__
 
@@ -39,3 +62,4 @@ typedef struct _nitf_TREDescriptionSet
 
 
 #endif
+
