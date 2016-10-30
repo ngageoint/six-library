75e46f46524a8b161ca2c06eb9b30feca0cf5236
Adam Sylvester <adam.sylvester@mdaus.com>
2016-10-30 07:10:03 -0400

    Squashed 'externals/nitro/' changes from 143b5a7..fe5317e
    fe5317e Merge pull request #24 from mdaus/adding-banners
    3191a59 Added banners
    84a6dea Merge pull request #23 from tclarke/master
    73b2ff1 Changed C cast to C++ static_cast
    0087652 Fix ambiguous cast on clang.
    
    git-subtree-dir: externals/nitro
    git-subtree-split: fe5317e5e7c1e81c102449aeebc6134ba589f3d8

--- modules/c/nitf/include/nitf/PluginRegistry.h
+++ modules/c/nitf/include/nitf/PluginRegistry.h
@@ -1,3 +1,26 @@
+/* =========================================================================
+ * This file is part of NITRO
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2016, MDA Information Systems LLC
+ *
+ * NITRO is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #ifndef __NITF_PLUGIN_REGISTRY_H__
 #define __NITF_PLUGIN_REGISTRY_H__
 
@@ -200,3 +223,4 @@ nitf_PluginRegistry_retrieveCompInterface(const char *comp,
 NITF_CXX_ENDGUARD
 
 #endif
+
--- modules/c/nrt/source/IOInterface.c
+++ modules/c/nrt/source/IOInterface.c
@@ -1,3 +1,26 @@
+/* =========================================================================
+ * This file is part of NITRO
+ * =========================================================================
+ *
+ * (C) Copyright 2004 - 2016, MDA Information Systems LLC
+ *
+ * NITRO is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #include "nrt/IOInterface.h"
 
 NRT_CXX_GUARD typedef struct _IOHandleControl
@@ -425,3 +448,4 @@ NRTAPI(nrt_IOInterface *) nrt_BufferAdapter_construct(char *buf, size_t size,
 }
 
 NRT_CXX_ENDGUARD
+
