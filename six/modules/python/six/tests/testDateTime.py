2f6c4eab45e39ec7998567a3602ccc5e2a8396b1
JonathanMeans <jonathan.means@mdaus.com>
2016-11-17 11:34:09 -0500

    Add copyright

--- six/modules/python/six/tests/testDateTime.py
+++ six/modules/python/six/tests/testDateTime.py
@@ -1,3 +1,27 @@
+#!/user/bin/env python
+
+#
+# =========================================================================
+# This file is part of six-python
+# =========================================================================
+#
+# (C) Copyright 2004 - 2016, MDA Information Systems LLC
+#
+# six-python is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; If not,
+# see <http://www.gnu.org/licenses/>.
+#
+
 
 import sys
 from datetime import datetime
@@ -40,6 +64,5 @@ def run():
         sys.exit(0)
     sys.exit(1)
 
-if __name__ == '__main__':
     run()
 
