6e246c7c693fe5b190f2c4c0e273fc6cb7f94be3
Adam Sylvester <adam.sylvester@mdaus.com>
2016-12-18 09:29:30 -0500

    Cleanup - testing the same thing for file and NITF ISDs and taking inputs on the command line

--- six/projects/csm/tests/test_sicd_csm.cpp
+++ six/projects/csm/tests/test_sicd_csm.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 #include <sys/DLL.h>
 #include <sys/Conf.h>
@@ -11,16 +12,6 @@
 #include <RasterGM.h>
 #include <Plugin.h>
 #include <NitfIsd.h>
-/*
-#include "Error.h"
-#include "Warning.h"
-#include "Plugin.h"
-#include "RasterGM.h"
-#include "FourParameterCorrelationModel.h"
-#include "LinearDecayCorrelationModel.h"
-#include "Isd.h"
-#include "NitfIsd.h"
-#include "BytestreamIsd.h"*/
 
 namespace
 {
@@ -43,45 +34,150 @@ std::string toString(nitf::FileSecurity security)
             security.getSecurityControlNumber().toString();
 
     return str;
-
 }
+
+std::string toString(nitf::DESubheader subheader)
+{
+    std::ostringstream ostr;
+    ostr << subheader.getFilePartType().toString()
+         << subheader.getTypeID().toString()
+         << subheader.getVersion().toString()
+         << subheader.getSecurityClass().toString()
+         << toString(subheader.getSecurityGroup())
+         << std::setw(4) << std::setfill('0')
+         << static_cast<nitf::Uint32>(subheader.getSubheaderFieldsLength());
+
+    nitf::TRE tre = subheader.getSubheaderFields();
+    for (nitf::TRE::Iterator iter = tre.begin(); iter != tre.end(); ++iter)
+    {
+        nitf::Pair pair = *iter;
+        ostr << tre.getField(pair.getKey()).toString();
+    }
+
+    return ostr.str();
+}
+
+class Test
+{
+public:
+    Test(const std::string& sicdPathname,
+         const std::string& confDir,
+         const csm::Plugin& plugin) :
+        mSicdPathname(sicdPathname),
+        mPlugin(plugin)
+    {
+        // Read in the SICD XML
+        mXmlRegistry.addCreator(six::DataType::COMPLEX,
+                new six::XMLControlCreatorT<six::sicd::ComplexXMLControl>());
+
+        mReader.setXMLControlRegistry(&mXmlRegistry);
+
+        const std::string schemaDir =
+                sys::Path(confDir).join("schema").join("six");
+        mReader.load(mSicdPathname, std::vector<std::string>(1, schemaDir));
+        mComplexData = six::sicd::Utilities::getComplexData(mReader);
+    }
+
+    void testFileISD()
+    {
+        testISD(csm::Isd(mSicdPathname));
+    }
+
+    void testNitfISD()
+    {
+        csm::Nitf21Isd nitfIsd(mSicdPathname);
+
+        csm::Des des;
+
+        // NITRO parsed the subheader into a nice structure - need to grab all the
+        // fields and jam them back into a string like CSM wants
+        nitf::DESegment segment = static_cast<nitf::DESegment>(
+                mReader.getRecord().getDataExtensions().getFirst().getData());
+        des.setSubHeader(toString(segment.getSubheader()));
+
+        // The DES's data is just the SICD XML string
+        des.setData(six::toXMLString(mComplexData.get(), &mXmlRegistry));
+
+        nitfIsd.addFileDes(des);
+
+        testISD(nitfIsd);
+    }
+
+private:
+    void testISD(const csm::Isd& isd)
+    {
+        // Construct the model
+        // We take ownership of the memory for this
+        if (!mPlugin.canModelBeConstructedFromISD(isd, MODEL_NAME))
+        {
+            throw except::Exception(Ctxt("Can't construct ISD"));
+        }
+
+        std::auto_ptr<csm::RasterGM> model(reinterpret_cast<csm::RasterGM*>(
+                mPlugin.constructModelFromISD(isd, MODEL_NAME)));
+
+        const six::RowColInt scpPixel = mComplexData->imageData->scpPixel;
+        const six::SCP scp = mComplexData->geoData->scp;
+
+        csm::ImageCoord imagePt(scpPixel.row + 0.5, scpPixel.col + 0.5);
+        const double height = scp.llh.getAlt();
+
+        csm::EcefCoord coord = model->imageToGround(
+                imagePt,
+                height,
+                0);
+        std::cout << "CSM ECEF coord is " << coord.x << " " << coord.y << " " << coord.z << std::endl;
+
+        scene::Vector3 csmCoord;
+        csmCoord[0] = coord.x;
+        csmCoord[1] = coord.y;
+        csmCoord[2] = coord.z;
+        scene::Vector3 diff = scp.ecf - csmCoord;
+        std::cout << "Diff " << diff[0] << " " << diff[1] << " " << diff[2] << std::endl;
+
+        // Round trip it
+        csm::ImageCoord rtCoord = model->groundToImage(coord, 0);
+        std::cout << "Got back " << rtCoord.line << " " << rtCoord.samp << std::endl;
+    }
+
+private:
+    static const char MODEL_NAME[];;
+
+    const std::string mSicdPathname;
+    const csm::Plugin& mPlugin;
+
+    six::XMLControlRegistry mXmlRegistry;
+    six::NITFReadControl mReader;
+    std::auto_ptr<six::sicd::ComplexData> mComplexData;
+};
+
+const char Test::MODEL_NAME[] = "SICD_SENSOR_MODEL";
 }
 
 int main(int argc, char** argv)
 {
     try
     {
-        sys::DLL dll("C:\\Users\\Adam\\Documents\\Code\\checkout\\six\\install\\share\\CSM\\plugins\\SIX_SAR_MDAIS_112_winVC140_CSM301.dll");
+        // Parse the command line
+        if (argc != 4)
+        {
+            std::cerr << "Usage: " << sys::Path::basename(argv[0])
+                      << " <SIX CSM DLL pathname> <SICD pathname>"
+                      << " <SIX conf directory>\n\n";
+            return 1;
+        }
+        const std::string dllPathname(argv[1]);
+        const std::string sicdPathname(argv[2]);
+        const std::string confDir(argv[3]);
 
-        const std::string sicdPathname("C:\\Users\\Adam\\Downloads\\sicd_example_RMA_RGZERO_RE32F_IM32F.nitf");
-
-        const std::string confDir("C:\\Users\\Adam\\Documents\\Code\\checkout\\six\\install\\conf");
-
-        const std::string schemaDir(sys::Path(confDir).join("schema").join("six"));
-
-        // TODO: Fill me in!!
-        std::vector<std::string> schemaDirs(1, schemaDir);
-
-        /*
-        std::auto_ptr<six::sicd::ComplexData> complexData(
-                six::sicd::Utilities::getComplexData(sicdPathname, schemaDirs));*/
-
-        six::XMLControlRegistry xmlRegistry;
-        xmlRegistry.addCreator(six::DataType::COMPLEX,
-                new six::XMLControlCreatorT<six::sicd::ComplexXMLControl>());
-
-        six::NITFReadControl reader;
-        reader.setXMLControlRegistry(&xmlRegistry);
-        reader.load(sicdPathname, schemaDirs);
-        std::auto_ptr<six::sicd::ComplexData> complexData(
-                six::sicd::Utilities::getComplexData(reader));
-
-        std::string xmlStr = six::toXMLString(complexData.get(), &xmlRegistry);
-
-        //std::cout << "xHey: " << xmlStr << std::endl;
-
+        // Load the SIX CSM DLL
+        // Quite frankly I don't know by what magic csm::Plugin::getList() finds
+        // it - some global is getting set automatically when the DLL is just
+        // opened that it's finding
+        sys::DLL dll(dllPathname);
         csm::Plugin::setDataDirectory(confDir);
 
+        // Make sure we found it
         csm::PluginList pluginList = csm::Plugin::getList();
 
         if (pluginList.size() != 1)
@@ -98,108 +194,26 @@ int main(int argc, char** argv)
                     "Unexpected plugin name '" + plugin.getPluginName() + "'"));
         }
 
-        std::cout << "Hey: " << plugin.getPluginName() << std::endl;
-        std::cout << "Model: " << plugin.getModelName(0) << std::endl;
+        Test test(sicdPathname, confDir, plugin);
+        test.testFileISD();
+        test.testNitfISD();
 
-
-        // Set data directory!!
-
-        // Try out the file ISD version
-        //csm::Isd fileIsd(sicdPathname);
-        csm::Isd fileIsd(sicdPathname);
-        std::cout << "I constructed the ISD" << std::endl;
-        std::cout << "It has a name of " << fileIsd.filename() << std::endl;
-
-        if (plugin.canModelBeConstructedFromISD(fileIsd, "SICD_SENSOR_MODEL"))
-        {
-            std::cout << "Yep - can construct it!" << std::endl;
-        }
-        else
-        {
-            std::cout << "Nope - can't construct it" << std::endl;
-        }
-
-        std::auto_ptr<csm::RasterGM> model(reinterpret_cast<csm::RasterGM*>(
-                plugin.constructModelFromISD(fileIsd, "SICD_SENSOR_MODEL")));
-
-        //4839.5 4299.5 488.0295281264820
-        csm::ImageCoord imagePt(4839.5, 4299.5);
-        double height = 488.0295281264820;
-
-        csm::EcefCoord coord = model->imageToGround(
-                imagePt,
-                height,
-                0);
-        std::cout << "I got " << coord.x << " " << coord.y << " " << coord.z << std::endl;
-
-        // NITF ISD
-        csm::Nitf21Isd nitfIsd(sicdPathname);
-
-        csm::Des des;
-
-        nitf::DESegment segment = static_cast<nitf::DESegment>(
-                reader.getRecord().getDataExtensions().getFirst().getData());
-        nitf::DESubheader subheader = segment.getSubheader();
-
-        std::ostringstream ostr;
-        ostr << subheader.getFilePartType().toString()
-             << subheader.getTypeID().toString()
-             << subheader.getVersion().toString()
-             << subheader.getSecurityClass().toString()
-             << toString(subheader.getSecurityGroup())
-             << std::setw(4) << std::setfill('0')
-             << static_cast<nitf::Uint32>(subheader.getSubheaderFieldsLength());
-
-        nitf::TRE tre = subheader.getSubheaderFields();
-        for (nitf::TRE::Iterator iter = tre.begin(); iter != tre.end(); ++iter)
-        {
-            nitf::Pair pair = *iter;
-            std::cout << "Key:  '" << pair.getKey() << "'" << std::endl;
-            std::cout << "Data: '" << tre.getField(pair.getKey()).toString() << "'" << std::endl;
-
-            ostr << tre.getField(pair.getKey()).toString();
-        }
-
-        const std::string subheaderStr = ostr.str();
-        std::cout << "Total str: '" << subheaderStr << "'  length " << subheaderStr.length() << std::endl;
-
-        des.setSubHeader(subheaderStr);
-
-        des.setData(xmlStr);
-
-        nitfIsd.addFileDes(des);
-
-        if (plugin.canModelBeConstructedFromISD(nitfIsd, "SICD_SENSOR_MODEL"))
-        {
-            std::cout << "Yep - can construct it!" << std::endl;
-        }
-        else
-        {
-            std::cout << "Nope - can't construct it" << std::endl;
-        }
-
-        {
-        std::auto_ptr<csm::RasterGM> model(reinterpret_cast<csm::RasterGM*>(
-                plugin.constructModelFromISD(nitfIsd, "SICD_SENSOR_MODEL")));
-
-        //std::cout << "Hey: " << (void*)model << std::endl;
-
-        csm::EcefCoord coord = model->imageToGround(
-                imagePt,
-                height,
-                0);
-        std::cout << "second time I got " << coord.x << " " << coord.y << " " << coord.z << std::endl;
-        }
-
-        // Try out the Nitf21Isd
-
-        // See vts_isd.cpp's initNitf21ISD() for how to do this - you need to
-        // fill it out yourself!
-        // We can be lazy though - it's only the DES data that we need to populate!
-
+        return 0;
+    }
+    // TODO: At least on Windows we don't ever seem to actually get exceptions
+    //       that make it out of the plugin
+    catch (const std::exception& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
+    catch (const except::Exception& ex)
+    {
+        std::cerr << ex.toString() << std::endl;
     }
     catch (...)
     {
-
+        std::cerr << "Unknown exception\n";
     }
+
+    return 1;
 }
