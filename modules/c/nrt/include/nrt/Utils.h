c6fd10bbd9cc3003151eb9fe8790f69072be1dec
Jonathan Means <jmeans@mdaus.com>
2016-12-20 11:47:10 -0500

    Use __inline instead of inline on Windows to deal with how VS deals with C code

--- modules/c/nrt/include/nrt/Utils.h
+++ modules/c/nrt/include/nrt/Utils.h
@@ -181,8 +181,13 @@ NRTAPI(char) nrt_Utils_cornersTypeAsCoordRep(nrt_CornersType type);
  * \param indexOne Index of first byte to be swapped
  * \param indexTwo Index of second byte to be swapped
  */
+ #ifdef WIN32
+NRTPRIV(void) __inline nrt_Utils_swap(nrt_Uint8* value, size_t indexOne,
+        size_t indexTwo)
+#else
 NRTPRIV(void) inline nrt_Utils_swap(nrt_Uint8* value, size_t indexOne,
         size_t indexTwo)
+#endif		
 {
     nrt_Uint8 temp;
     temp = value[indexOne];
