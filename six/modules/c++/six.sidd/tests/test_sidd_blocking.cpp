3b8f34499e3d0305db0a1c6dd0f47017e260c802
JonathanMeans <jonathan.means@MDAUS.com>
2017-02-21 14:21:36 -0500

    Add diagnostic

--- six/modules/c++/six.sidd/tests/test_sidd_blocking.cpp
+++ six/modules/c++/six.sidd/tests/test_sidd_blocking.cpp
@@ -76,13 +76,10 @@ std::string getProgramPathname(const std::string& installPathname,
         join("bin").join(programName));
 
     // Clean it up so path is readable
-    if (str::contains(testPathname, "-"))
-    {
-       str::replaceAll(testPathname, "-", "^-");
-    }
     if (str::contains(testPathname, " "))
     {
         testPathname = "\"" + testPathname + "\"";
+        std::cerr << "319: " << testPathname << std::endl;
     }
    
     if (!sys::OS().exists(testPathname))
