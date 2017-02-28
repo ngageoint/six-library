3aef0e4d797e98dbeea778ba3dec349c08a2a769
JonathanMeans <jonathan.means@mdaus.com>
2017-02-28 13:57:32 -0500

    Unwrapping seems to work

--- six/modules/python/six/source/wrapper.py
+++ six/modules/python/six/source/wrapper.py
@@ -2,12 +2,14 @@ import inspect
 import numpy
 import types
 from coda.math_poly import Poly1D, Poly2D
+from pysix.six_sicd import *
 
 def getMemberVariables(obj):
     members = []
     for attribute in dir(obj):
         value = getattr(obj, attribute)
         if (not inspect.ismethod(value) and
+            not inspect.isfunction(value) and
             not attribute.startswith('_') and
             not isinstance(value, types.BuiltinFunctionType) and
             not isinstance(value, types.BuiltinMethodType)):
@@ -26,7 +28,8 @@ def isSmartPointer(value):
             'ScopedCopyable' in type(value).__name__)
 
 def isVector(value):
-    return 'Vector' in type(value).__name__
+    return ('Vector' in type(value).__name__ and
+            'Vector3' not in type(value).__name__)
 
 def isLikeAPrimitive(value):
     return isinstance(value, (types.IntType, types.FloatType, types.StringType,
@@ -55,4 +58,70 @@ def wrap(obj):
     for (name, value) in getMemberVariables(obj):
         setattr(wrapper, name, wrap(value))
     return wrapper
+
+def unwrapPoly(wrapper, source):
+    if 'Poly1D' in type(source).__name__:
+        source = Poly1D.fromArray(wrapper)
+    elif 'Poly2D' in type(source).__name__:
+        source = Poly2D.fromArray(wrapper)
+    else:
+        raise Exception('Could not recognize {0} as Polynomial'.format(
+                type(source).__name__))
+
+    return source
+
+def unwrapSmartPointer(wrapper, source):
+    if wrapper is None:
+        source.reset()
+    else:
+        constructorName = 'make' + type(source).__name__
+        source = globals()[constructorName]()
+        unwrap(wrapper, source)
+    return source
+
+def unwrapToVector(wrapper, source):
+    # The extra element is used to figured out
+    # type stuff during further unwrapping
+    # We discard it at the end
+    source.resize(len(wrapper))
+    sampleElement = source[0]
+
+    if isSmartPointer(sampleElement):
+        constructorName = type(sampleElement).__name__
+        constructorName = 'make' + constructorName
+        for ii in range(len(wrapper)):
+            sourceElement = globals()[constructorName]()
+            element = wrapper[ii]
+            if element is None:
+                sourceElement.reset()
+            else:
+                unwrap(element, sourceElement)
+        source[ii] = sourceElement
+    else:
+        for ii in range(len(wrapper)):
+            source[ii] = unwrap(wrapper[ii], source[ii])
+    return source
+
+def unwrap(wrapper, source):
+    if isLikeAPrimitive(source):
+        return wrapper
+    for (name, unwrappedValue) in getMemberVariables(source):
+        wrappedValue = getattr(wrapper, name)
+        if isLikeAPrimitive(unwrappedValue):
+            unwrappedValue = wrappedValue
+        elif 'PolyVector3' in type(unwrappedValue).__name__:
+            unwrappedValue = wrappedValue
+        elif isVector(unwrappedValue):
+            if len(wrappedValue) == 0:
+                unwrappedValue.clear()
+            else:
+                unwrappedValue = unwrapToVector(wrappedValue, unwrappedValue)
+        elif isSmartPointer(unwrappedValue):
+            unwrappedValue = unwrapSmartPointer(wrappedValue, unwrappedValue)
+        elif isPoly(unwrappedValue):
+            unwrappedValue = unwrapPoly(wrappedValue, unwrappedValue)
+        else:
+            unwrap(wrappedValue, unwrappedValue)
+        setattr(source, name, unwrappedValue)
+    return source
 
--- six/modules/python/six/unittests/testWrapper.py
+++ six/modules/python/six/unittests/testWrapper.py
@@ -3,10 +3,8 @@ import unittest
 import numpy
 
 from coda.math_poly import Poly1D, Poly2D
-from pysix.wrapper import wrap
+from pysix.wrapper import wrap, unwrap
 from pysix.six_sicd import *
-
-
 
 class TestWrapping(unittest.TestCase):
 
@@ -81,7 +79,92 @@ class TestWrapping(unittest.TestCase):
         self.assertEqual(wrappedObject.geoInfos[2].name, "third name")
 
 
+class TestUnwrapping(unittest.TestCase):
+
+    def testCanUnwrapPrimitives(self):
+        originalObject = HalfPowerBeamwidths()
+        wrappedObject = wrap(originalObject)
+        wrappedObject.dcx = 5.0
+        wrappedObject.dcy = 3.4
+
+        unwrap(wrappedObject, originalObject)
+
+        self.assertEqual(originalObject.dcx, 5)
+        self.assertEqual(originalObject.dcy, 3.4)
+
+    def testUnwrappingPoly1D(self):
+        originalObject = ElectricalBoresight()
+        originalObject.dcxPoly = Poly1D(1)
+        originalObject.dcxPoly[0] = 3
+        wrappedObject = wrap(originalObject)
+        wrappedObject.dcxPoly = numpy.zeros(0)
+        wrappedObject.dcyPoly = numpy.ones(1)
+
+        unwrap(wrappedObject, originalObject)
+
+        self.assertTrue(originalObject.dcxPoly.empty())
+        self.assertEqual(originalObject.dcyPoly[0], 1)
+        self.assertIsInstance(originalObject.dcxPoly, Poly1D)
+
+    def testUnwrappingPoly2D(self):
+        originalObject = GainAndPhasePolys()
+        originalObject.gainPoly = Poly2D(1, 1)
+        wrappedObject = wrap(originalObject)
+        wrappedObject.gainPoly = numpy.zeros(0)
+        wrappedObject.phasePoly = numpy.ones((1, 1))
+
+        unwrap(wrappedObject, originalObject)
+
+        self.assertIsInstance(originalObject.gainPoly, Poly2D)
+        self.assertTrue(originalObject.gainPoly.empty())
+        self.assertIsInstance(originalObject.phasePoly, Poly2D)
+        self.assertEqual(originalObject.phasePoly[(0,0)], 1)
+
+    def testCanUnwrapSmartPointer(self):
+        originalObject = AntennaParameters()
+        originalObject.electricalBoresight = (
+                makeScopedCopyableElectricalBoresight())
+        wrappedObject = wrap(originalObject)
+        wrappedObject.electricalBoresight = None
+        wrappedObject.halfPowerBeamwidths = (
+                makeScopedCopyableHalfPowerBeamwidths())
+        wrappedObject.halfPowerBeamwidths.dcx = 3
+
+        unwrap(wrappedObject, originalObject)
+
+        self.assertIsNone(originalObject.electricalBoresight.get())
+        self.assertIsNotNone(originalObject.halfPowerBeamwidths.get())
+        self.assertEqual(originalObject.halfPowerBeamwidths.dcx, 3)
+
+    def testUnwrapList(self):
+        originalObject = CollectionInformation()
+        originalObject.countryCodes.push_back('element')
+        wrappedObject = wrap(originalObject)
+        wrappedObject.countryCodes.append('secondElement')
+
+        unwrap(wrappedObject, originalObject)
+
+        self.assertIn('Vector', type(originalObject.countryCodes).__name__)
+        self.assertEqual(originalObject.countryCodes[1], 'secondElement')
+
+    def testUnwrapVectorOfSmartPointers(self):
+        originalObject = GeoData()
+        originalObject.geoInfos.resize(3)
+        originalObject.geoInfos[0] = makeScopedCloneableGeoInfo()
+        originalObject.geoInfos[0].name = "first name"
+        originalObject.geoInfos[2] = makeScopedCloneableGeoInfo()
+        originalObject.geoInfos[2].name = "third name"
+        wrappedObject = wrap(originalObject)
+        wrappedObject.geoInfos[2] = None
+        wrappedObject.geoInfos[1] = makeScopedCloneableGeoInfo()
+        wrappedObject.geoInfos[1].name = "second name"
+
+        unwrap(wrappedObject, originalObject)
+
+        self.assertEqual(originalObject.geoInfos.size(), 3)
+        self.assertEqual(wrappedObject.geoInfos[1].name, "second name")
+        self.assertIsNone(wrappedObject.geoInfos[2])
+
 if __name__ == '__main__':
     unittest.main()
-    print('here below')
 
