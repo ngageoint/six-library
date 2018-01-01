970abdb2cdaad42361748dcf62cfea8d337ac553
Adam Sylvester <adam.sylvester@mdaus.com>
2018-01-01 17:32:27 -0500

    Fixing banner

--- six/modules/c++/six.sicd/tests/test_sicd_byte_provider.cpp
+++ six/modules/c++/six.sicd/tests/test_sicd_byte_provider.cpp
@@ -4,7 +4,7 @@
  *
  * (C) Copyright 2004 - 2017, MDA Information Systems LLC
  *
- * six-c++ is free software; you can redistribute it and/or modify
+ * six.sicd-c++ is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
