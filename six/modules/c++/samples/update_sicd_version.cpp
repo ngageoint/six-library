dc7f1f18d5b10833bf64c5113d92d5be43fbf16a
Jonathan Means <Jonathan.Means@Maxar.com>
2020-02-03 12:10:27 -0500

    Fix copyright block

--- six/modules/c++/samples/update_sicd_version.cpp
+++ six/modules/c++/samples/update_sicd_version.cpp
@@ -1,10 +1,10 @@
 /* =========================================================================
- * This file is part of six.sicd-c++
+ * This file is part of six-c++
  * =========================================================================
  *
  * (C) Copyright 2004 - 2020, MDA Information Systems LLC
  *
- * six.sicd-c++ is free software; you can redistribute it and/or modify
+ * six-c++ is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
