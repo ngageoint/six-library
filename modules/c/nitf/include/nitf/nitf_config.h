c7a30575f7015d6aa757803fdcbb35a74756224d
Jonathan Means <Jonathan.Means@Maxar.com>
2020-02-21 12:29:48 -0500

    CMake build for c/c++ modules

---
+++ modules/c/nitf/include/nitf/nitf_config.h
@@ -0,0 +1,6 @@
+#ifndef NITF_CONFIG_H
+#define NITF_CONFIG_H
+
+#define NITF_LIB_VERSION "2.8"
+
+#endif
---
+++ modules/c/nrt/include/nrt/nrt_config.h
@@ -0,0 +1,8 @@
+#ifndef NRT_CONFIG_H
+#define NRT_CONFIG_H
+
+#define NRT_LIB_VERSION 2.8
+#define HAVE_SYS_TIME_H 1
+#define HAVE_CLOCK_GETTIME 1
+
+#endif
