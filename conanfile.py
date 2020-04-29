ba8911ddec5be11a8651129c36a59d37bcb8d450
Keith Wilhelm <1964653+kdwilhelm@users.noreply.github.com>
2020-04-29 18:29:02 -0400

    Refactor and clean up CMake and Conan builds

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,23 @@
 from conans import ConanFile, CMake, tools
+import os
+import sys
 
 class SixConan(ConanFile):
     name = "six-library"
     url = "https://github.com/ngageoint/six-library"
     description = "library for reading and writing the Sensor Independent XML (SIX) format"
     settings = "os", "compiler", "build_type", "arch"
-    requires = ("coda-oss/CMake_update_win_ede7427059e489bc@user/testing",
-                "nitro/cmake-integration_ba72c0500fd4824a@user/testing")
+    requires = ("coda-oss/master_67d6362bcfcf07e2",
+                "nitro/master_038069b2eaea5981")
     options = {"shared": [True, False],
+               "PYTHON_HOME": "ANY",
+               "PYTHON_VERSION": "ANY",
+               "ENABLE_PYTHON": [True, False],
                }
     default_options = {"shared": False,
+                       "PYTHON_HOME": "",
+                       "PYTHON_VERSION": "",
+                       "ENABLE_PYTHON": True,
                        }
     exports_sources = ("CMakeLists.txt",
                        "LICENSE",
@@ -20,14 +28,28 @@ class SixConan(ConanFile):
     generators = "cmake_paths"
     license = "GNU LESSER GENERAL PUBLIC LICENSE Version 3"
 
+    # default to short_paths mode (Windows only)
+    short_paths = True
+    # default the short_paths home to ~/.conan_short
+    # this may be overridden by setting the environment variable
+    # CONAN_USER_HOME_SHORT or setting user_home_short in ~/.conan/conan.conf
+    if sys.platform.startswith('win32') and os.getenv("CONAN_USER_HOME_SHORT") is None:
+        os.environ["CONAN_USER_HOME_SHORT"] = os.path.join(
+            os.path.expanduser("~"), ".conan_short")
+
     def set_version(self):
         git = tools.Git(folder=self.recipe_folder)
         self.version = "%s_%s" % (git.get_branch(), git.get_revision()[:16])
 
+    def configure(self):
+        # Python-related options are forced to be the same for dependencies
+        for dep in ['coda-oss', 'nitro']:
+            self.options[dep].PYTHON_HOME = self.options.PYTHON_HOME
+            self.options[dep].PYTHON_VERSION = self.options.PYTHON_VERSION
+            self.options[dep].ENABLE_PYTHON = self.options.ENABLE_PYTHON
+
     def _configure_cmake(self):
         cmake = CMake(self)
-        if self.settings.compiler == "Visual Studio":
-            cmake.definitions["CMAKE_BUILD_TYPE"] = self.settings.build_type
         cmake.configure()
         return cmake
 
