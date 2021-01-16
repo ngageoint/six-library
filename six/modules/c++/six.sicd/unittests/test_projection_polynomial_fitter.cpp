079c211e87339ec24d9d20f8d0e5fdc03779ca54
Dan Smith <Dan.Smith@maxar.com>
2021-01-16 18:25:48 -0500

    fix build error on Linux

--- six/modules/c++/six.sicd/unittests/test_projection_polynomial_fitter.cpp
+++ six/modules/c++/six.sicd/unittests/test_projection_polynomial_fitter.cpp
@@ -50,7 +50,7 @@ loadPolynomialFitter(const fs::path& exePath)
     }
 
     std::vector<std::string> schemaPaths;
-    std::unique_ptr<six::sicd::ComplexData> complexData;
+    mem::auto_ptr<six::sicd::ComplexData> complexData;
     std::vector<std::complex<float> > buffer;
     six::sicd::Utilities::readSicd(sicdPathname, schemaPaths, complexData, buffer);
     return six::sicd::Utilities::getPolynomialFitter(*complexData);
