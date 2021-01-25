7397b358cce83e8e12f8dc472b4b2ae4a34934ac
Dan Smith <dan.smith@maxar.com>
2021-01-25 14:45:24 -0500

    fix interleaved() template

--- six/modules/c++/six/include/six/ReadControl.h
+++ six/modules/c++/six/include/six/ReadControl.h
@@ -145,9 +145,25 @@ struct ReadControl
      * \return Buffer of image data.  This is simply equal to buffer.get() and
      * is provided as a convenience.
      */
+#if !CODA_OSS_cpp17
     template<typename T>
     T* interleaved(Region& region, size_t imageNumber,
-           mem::auto_ptr<T[]>& buffer)
+           std::auto_ptr<T[]>& buffer)
+    {
+        buffer.reset(reinterpret_cast<T*>(interleaved(region, imageNumber)));
+        return buffer.get();
+    }
+#endif
+    template<typename T>
+    T* interleaved(Region& region, size_t imageNumber,
+        mem::ScopedArray<T>& buffer)
+    {
+        buffer.reset(reinterpret_cast<T*>(interleaved(region, imageNumber)));
+        return buffer.get();
+    }
+    template<typename T>
+    T* interleaved(Region& region, size_t imageNumber,
+        std::unique_ptr<T[]>& buffer)
     {
         buffer.reset(reinterpret_cast<T*>(interleaved(region, imageNumber)));
         return buffer.get();
