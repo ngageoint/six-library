96918daf007397605a14914ae2645d98e0e0fec2
Dan Smith <dan.smith@maxar.com>
2021-01-30 15:58:51 -0500

    std::filesystem::path doesn't implicitly convert to std::string

--- six/modules/c++/samples/test_extract_xml.cpp
+++ six/modules/c++/samples/test_extract_xml.cpp
@@ -72,7 +72,7 @@ int main(int argc, char** argv)
         //! Fill out basename if not user specified
         if (basename.empty())
         {
-            basename = fs::path(inputFile).stem();
+            basename = fs::path(inputFile).stem().string();
         }
 
         nitf::Reader reader;
