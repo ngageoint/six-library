8827b2d445df7363e4c779f38ed487b3e5e6fe65
Dan Smith <dan.smith@maxar.com>
2021-11-15 09:46:30 -0500

    use template for some unit-test macros

--- Test/Test.h
+++ Test/Test.h
@@ -5,16 +5,32 @@
 // use GTest macros to mimic existing unit-tests
 
 #define TEST_ASSERT(X) EXPECT_TRUE(X)
-#define TEST_ASSERT_NULL(X) TEST_ASSERT((X) == NULL)
-#define TEST_ASSERT_TRUE(X) TEST_ASSERT((X) == true)
-#define TEST_ASSERT_FALSE(X) TEST_ASSERT((X) == false)
 
-#define TEST_ASSERT_EQ(X1, X2) EXPECT_EQ(X1, X2)
+template<typename T, typename U>
+inline void test_assert_eq_(const T& t, const U& u)
+{
+	EXPECT_EQ(t, u);
+}
+#define TEST_ASSERT_EQ(X1, X2) { test_assert_eq_(X1, X2); }
 #define TEST_ASSERT_EQ_INT(X1, X2) TEST_ASSERT_EQ(X1, X2)
-#define TEST_ASSERT_EQ_STR(X1, X2) EXPECT_STREQ(X1, X2)
+template<typename T, typename U>
+inline void test_assert_str_eq_(const T& t, const U& u)
+{
+	EXPECT_STREQ(t, u);
+}
+#define TEST_ASSERT_EQ_STR(X1, X2) test_assert_str_eq_(X1, X2)
 #define TEST_ASSERT_EQ_FLOAT(X1, X2) EXPECT_FLOAT_EQ(static_cast<float>(X1), static_cast<float>(X2))
 
-#define TEST_ASSERT_NOT_EQ(X1, X2) EXPECT_NE(X1, X2)
+#define TEST_ASSERT_NULL(X) TEST_ASSERT_EQ((X), nullptr)
+#define TEST_ASSERT_TRUE(X) TEST_ASSERT_EQ((X), true)
+#define TEST_ASSERT_FALSE(X) TEST_ASSERT_EQ((X), false)
+
+template<typename T, typename U>
+inline void test_assert_not_eq_(const T& t, const U& u)
+{
+	EXPECT_NE(t, u);
+}
+#define TEST_ASSERT_NOT_EQ(X1, X2) { test_assert_not_eq_(X1, X2); }
 
 #define TEST_ASSERT_GREATER(X1, X2) EXPECT_GT(X1, X2)
 
