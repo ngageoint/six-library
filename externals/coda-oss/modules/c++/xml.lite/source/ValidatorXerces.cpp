74ce242cf352a333684379cc9bd0278c0cf57571
Dan Smith <dan.smith@maxar.com>
2021-12-29 09:01:34 -0500

    latest from coda-oss

--- externals/coda-oss/modules/c++/xml.lite/source/ValidatorXerces.cpp
+++ externals/coda-oss/modules/c++/xml.lite/source/ValidatorXerces.cpp
@@ -21,10 +21,12 @@
  */
 
 #include <xml/lite/xml_lite_config.h>
-
 #ifdef USE_XERCES
-
 #include <xml/lite/ValidatorXerces.h>
+
+#include <algorithm>
+#include <iterator>
+
 #include <sys/OS.h>
 #include <io/StringStream.h>
 #include <mem/ScopedArray.h>
@@ -72,7 +74,20 @@ bool ValidationErrorHandler::handleError(
     return true;
 }
 
-
+inline std::vector<std::string> convert(const std::vector<sys::Filesystem::path>& schemaPaths)
+{
+    std::vector<std::string> retval;
+    std::transform(schemaPaths.begin(), schemaPaths.end(), std::back_inserter(retval),
+                   [](const sys::Filesystem::path& p) { return p.string(); });
+    return retval;
+}
+ValidatorXerces::ValidatorXerces(
+        const std::vector<sys::Filesystem::path>& schemaPaths,
+        logging::Logger* log,
+        bool recursive) :
+    ValidatorXerces(convert(schemaPaths), log, recursive)
+{
+}
 ValidatorXerces::ValidatorXerces(
     const std::vector<std::string>& schemaPaths, 
     logging::Logger* log,
