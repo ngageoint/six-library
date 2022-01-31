e4b6eb9c80c7e105c705ca2d6b816cb1894c34f1
Dan Smith <dan.smith@maxar.com>
2022-01-31 11:32:10 -0500

    build in VS2022, remove compiler warnings

--- six/modules/c++/six.sicd/include/six/sicd/ComplexToAMP8IPHS8I.h
+++ six/modules/c++/six.sicd/include/six/sicd/ComplexToAMP8IPHS8I.h
@@ -1,5 +1,29 @@
-#ifndef __SIX_SICD_COMPLEXTOAMP8IPHS8I__
-#define __SIX_SICD_COMPLEXTOAMP8IPHS8I__
+/* =========================================================================
+* This file is part of six.sicd-c++
+* =========================================================================
+*
+* (C) Copyright 2022, Maxar Technologies, Inc.
+*
+* six.sicd-c++ is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this program; If not,
+* see <http://www.gnu.org/licenses/>.
+*
+*/
+#ifndef SIX_six_sicd_ComplexToAMP8IPHS8I_h_INCLUDED_
+#define SIX_six_sicd_ComplexToAMP8IPHS8I_h_INCLUDED_
+#pragma once
+
+#include <stdint.h>
 
 #include <six/sicd/ImageData.h>
 
@@ -10,8 +34,8 @@ namespace sicd
 /*!
  * \brief A utility that's used to convert complex values into 8-bit amplitude and phase values.
  */
-class ComplexToAMP8IPHS8I {
-public:
+struct ComplexToAMP8IPHS8I final
+{
     /*!
      * Create a lookup structure that converts from complex to amplitude and phase.
      * @param pAmplitudeTable optional amplitude table.
@@ -23,23 +47,16 @@ public:
      * @param v complex value to query with
      * @return nearest amplitude and phase value
      */
-    ImageData::AMP8I_PHS8I_t nearest_neighbor(const std::complex<double>& v) const;
+    ImageData::AMP8I_PHS8I_t nearest_neighbor(const std::complex<float>& v) const;
 
 private:
     //! The sorted set of possible magnitudes order from small to large.
-    std::array<double, UINT8_MAX + 1> magnitudes;
+    std::array<long double, UINT8_MAX + 1> magnitudes;
     //! The difference in phase angle between two UINT phase values.
-    double phase_delta;
+    long double phase_delta;
     //! Unit vector rays that represent each direction that phase can point.
-    std::array<std::complex<double>, UINT8_MAX + 1> phase_directions;
-
-    /*!
-     * Get the phase of a complex value.
-     * @param v complex value
-     * @return phase between [0, 2PI]
-     */
-    static double GetPhase(const std::complex<double>& v);
+    std::array<std::complex<long double>, UINT8_MAX + 1> phase_directions;
 };
 }
 }
-#endif
+#endif // SIX_six_sicd_ComplexToAMP8IPHS8I_h_INCLUDED_
