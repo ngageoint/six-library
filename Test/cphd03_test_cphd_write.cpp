e39a44d96efb5b06806519f9ff52d2f17d95a45b
J. Daniel Smith <dan@jdanielsmith.me>
2022-06-15 14:39:18 -0400

    remove chpd03 GTest unittests

--- Test/cphd03_test_cphd_write.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-
-#include "cphd03_Test.h"
-
-#define CODA_OSS_testCase_(X)  testCase ## _ ## X
-#define TEST_CASE(X) void CODA_OSS_testCase_(X)(std::string); TEST(cphd03_test_cphd_write, X) { CODA_OSS_testCase_(X)(#X); } void CODA_OSS_testCase_(X)(std::string testName)
-#include "six/modules/c++/cphd03/unittests/test_cphd_write.cpp"
