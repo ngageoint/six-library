145fe74d9bba8beab29841f88f4998cb8498eee7
Dan Smith <dan.smith@maxar.com>
2022-06-28 17:49:59 -0400

    "UnitTest" not "Test"

--- Test/cphd_Test.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#if defined(GTEST_API_)
-#define __TEST_H__ // #include "../nitf/unittests/Test.h"
-#define __TEST_CASE_H__ // #include "../nitf/unittests/TestCase.h"
-#endif
-#include "Test.h"
--- Test/six.sidd_test_geometric_chip.cpp
+++
@@ -1,6 +0,0 @@
-#include "pch.h"
-
-#include "six_Test.h"
-
-#define TEST_CASE(X) TEST(sidd_test_geometric_chip, X)
-#include "six/modules/c++/six.sidd/unittests/test_geometric_chip.cpp"
