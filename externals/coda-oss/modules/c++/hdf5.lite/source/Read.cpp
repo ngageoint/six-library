5cbfd386ff05f87dae63ae05eb91de5c4c522072
Dan Smith <Dan.Smith@maxar.com>
2023-01-10 13:43:52 -0500

    Squashed 'externals/nitro/' changes from 08cbb96f9..c71acc492
    c71acc492 latest from coda-oss (#526)
    
    git-subtree-dir: externals/nitro
    git-subtree-split: c71acc492f4e00dfd0a3d10147deea0f819135bd

--- externals/coda-oss/modules/c++/hdf5.lite/source/Read.cpp
+++ externals/coda-oss/modules/c++/hdf5.lite/source/Read.cpp
@@ -51,15 +51,14 @@ inline types::RowCol<size_t> readDataset_(const H5::DataSet& dataset, std::vecto
     static_assert(sizeof(float) * 8 == 32, "'float' should be 32-bits"); // IEEE_F32LE
     return readDatasetT(dataset, H5T_FLOAT, H5::PredType::IEEE_F32LE, result);
 }
-
 inline types::RowCol<size_t> readDataset_(const H5::DataSet& dataset, std::vector<double>& result)
 {
     static_assert(sizeof(double) * 8 == 64, "'double' should be 64-bits"); // IEEE_F64LE
     return readDatasetT(dataset, H5T_FLOAT, H5::PredType::IEEE_F64LE, result);
 }
-
+template<typename T>
 static types::RowCol<size_t> readFile_(const coda_oss::filesystem::path& fileName, const std::string& datasetName,
-                                std::vector<double>& result)
+                                std::vector<T>& result)
 {
     /*
      * Open the specified file and the specified dataset in the file.
@@ -68,8 +67,14 @@ static types::RowCol<size_t> readFile_(const coda_oss::filesystem::path& fileNam
     const auto dataset = file.openDataSet(datasetName);
     return readDataset_(dataset, result);
 }
+
 types::RowCol<size_t> hdf5::lite::readFile(coda_oss::filesystem::path fileName, std::string datasetName,
     std::vector<double>& result)
 {
-    return details::try_catch_H5Exceptions(readFile_, fileName, datasetName, result); 
+    return details::try_catch_H5Exceptions(readFile_<double>, __FILE__, __LINE__, fileName, datasetName, result); 
+}
+types::RowCol<size_t> hdf5::lite::readFile(coda_oss::filesystem::path fileName, std::string datasetName,
+    std::vector<float>& result)
+{
+    return details::try_catch_H5Exceptions(readFile_<float>, __FILE__, __LINE__, fileName, datasetName, result); 
 }
