e1c74b9d989bfb75f1d9459d7e0d04c0036e1788
J. Daniel Smith <dan@jdanielsmith.me>
2023-01-25 16:47:19 -0500

    latest from coda-oss and nitro

--- externals/coda-oss/modules/c++/coda_oss/include/coda_oss/type_traits.h
+++ externals/coda-oss/modules/c++/coda_oss/include/coda_oss/type_traits.h
@@ -29,21 +29,7 @@
 #include "coda_oss/namespace_.h"
 namespace coda_oss
 {
-// workaround missing "is_trivially_copyable" in g++ < 5.0
-// https://stackoverflow.com/a/31798726/8877
-#if defined(__GNUC__) && (__GNUC__ < 5)
-template <typename T>
-struct is_trivially_copyable final
-{
-    // This old Intel compiler has enough C++14 for our needs; see CPlusPlus.h
-    #if !(defined(__INTEL_COMPILER) && (__INTEL_COMPILER_BUILD_DATE <= 20151021))
-        static_assert(CODA_OSS_cplusplus < 201402L, "C++14 must have is_trivially_copyable.");
-    #endif
-    static constexpr bool value = __has_trivial_copy(T);
-};
-#else
 using std::is_trivially_copyable;
-#endif
 }
 
 #endif  // CODA_OSS_coda_oss_type_traits_h_INCLUDED_
