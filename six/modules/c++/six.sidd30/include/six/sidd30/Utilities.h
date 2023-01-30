9a0c703a989af3523cddf44a876932f687a504d6
Dan Smith <dan.smith@maxar.com>
2023-01-30 17:06:44 -0500

    compile SIDD30 Utilities.cpp

--- six/modules/c++/six.sidd30/include/six/sidd30/Utilities.h
+++ six/modules/c++/six.sidd30/include/six/sidd30/Utilities.h
@@ -24,12 +24,132 @@
 #pragma once
 
 #include <six/sidd/Utilities.h>
+#include <six/sidd30/DerivedData.h>
 
 namespace six
 {
-    namespace sidd30
-    {
-        using Utilities = six::sidd::Utilities;
-    }
+namespace sidd30
+{
+    using Product = six::sidd::Product;
+    using Collection = six::sidd::Collection;
+
+struct Utilities final
+{
+    // TODO: This is only needed because getSceneGeometry() isn't implemented
+    //       for all projection types
+    static scene::SideOfTrack getSideOfTrack(const DerivedData* derived);
+
+    static std::unique_ptr<scene::SceneGeometry>
+    getSceneGeometry(const DerivedData* derived);
+
+    static std::unique_ptr<scene::GridGeometry>
+    getGridGeometry(const DerivedData* derived);
+
+    static std::unique_ptr<scene::GridECEFTransform>
+    getGridECEFTransform(const DerivedData* derived);
+
+    static void setProductValues(Poly2D timeCOAPoly, PolyXYZ arpPoly,
+            ReferencePoint ref, const Vector3* row, const Vector3* col,
+            types::RgAz<double>res, Product* product);
+
+    static void setProductValues(Vector3 arpVel, Vector3 arpPos,
+            Vector3 refPos, const Vector3* row, const Vector3* col,
+            types::RgAz<double>res, Product* product);
+
+    static void setCollectionValues(Poly2D timeCOAPoly, PolyXYZ arpPoly,
+            ReferencePoint ref, const Vector3* row, const Vector3* col,
+            Collection* collection);
+
+    static void setCollectionValues(Vector3 arpVel, Vector3 arpPos,
+            Vector3 refPos, const Vector3* row, const Vector3* col,
+            Collection* collection);
+
+    static std::pair<six::PolarizationSequenceType, six::PolarizationSequenceType>
+            convertDualPolarization(six::DualPolarizationType pol);
+
+    static std::unique_ptr<scene::ProjectionModel>
+    getProjectionModel(const DerivedData* data);
+
+
+    /*!
+     * Create a fake SIDD that's populated enough for
+     * general testing code to run without throwing exceptions
+     *
+     * \return mock DerivedData object
+     */
+    static std::unique_ptr<DerivedData> createFakeDerivedData();
+    static std::unique_ptr<DerivedData> createFakeDerivedData(const std::string& strVersion);
+
+
+    /*
+    * Parses the XML in 'xmlStream' and converts it into a DerivedData object.
+    * Throws if the underlying type is not derived.
+    *
+    * \param xmlStream Input stream containing XML
+    * \param schemaPaths Schema path(s)
+    * \param log Logger
+    *
+    * \return Data representation of 'xmlStr'
+    */
+    static std::unique_ptr<DerivedData> parseData(
+        ::io::InputStream& xmlStream,
+        const std::vector<std::string>& schemaPaths,
+        logging::Logger& log);
+    static std::unique_ptr<DerivedData> parseData(::io::InputStream& xmlStream,
+        const std::vector<std::filesystem::path>*, logging::Logger&);
+
+    /*
+    * Parses the XML in 'pathname' and converts it into a DerivedData object.
+    * Throws if the underlying type is not derived.
+    *
+    * \param pathname File containing plain text XML (not a NITF)
+    * \param schemaPaths Schema path(s)
+    * \param log Logger
+    *
+    * \return Data representation of the contents of 'pathname'
+    */
+    static std::unique_ptr<DerivedData> parseDataFromFile(
+        const std::string& pathname,
+        const std::vector<std::string>& schemaPaths,
+        logging::Logger& log);
+    static std::unique_ptr<DerivedData> parseDataFromFile(const std::filesystem::path&,
+        const std::vector<std::filesystem::path>*, logging::Logger* pLogger = nullptr);
+
+    /*
+    * Parses the XML in 'xmlStr' and converts it into a DerivedData object.
+    *
+    * \param xmlStr XML document as a string
+    * \param schemaPaths Schema path(s)
+    * \param log Logger
+    *
+    * \return Data representation of 'xmlStr'
+    */
+    static std::unique_ptr<DerivedData> parseDataFromString(
+        const std::string& xmlStr,
+        const std::vector<std::string>& schemaPaths,
+        logging::Logger& log);
+    static std::unique_ptr<DerivedData> parseDataFromString(
+        const std::u8string& xmlStr,
+        const std::vector<std::filesystem::path>* pSchemaPaths,
+        logging::Logger* pLogger=nullptr);
+
+    /*
+     * Converts 'data' back into a formatted XML string
+     *
+     * \param data Representation of SIDD data
+     * \param schemaPaths Schema paths.  If empty, the SIX_SCHEMA_PATH
+     *  environment variable will be used.
+     * \param logger Logger.  If nullptr, no logger will be used.
+     *
+     * \return XML string representation of
+     *'data'
+     */
+    static std::string toXMLString(const DerivedData& data,
+            const std::vector<std::string>& schemaPaths,
+            logging::Logger* logger);
+    static std::u8string toXMLString(const DerivedData&,
+        const std::vector<std::filesystem::path>*, logging::Logger* pLogger = nullptr);
+};
+}
 }
 #endif // SIX_six_sidd30_Utilities_h_INCLUDED_
