aab67018052fa621c86f9d07dd466e7d5de762e6
Dan Smith <dan.smith@maxar.com>
2023-02-07 09:01:34 -0500

    ism:13

--- six/modules/c++/six.sidd/unittests/test_valid_sixsidd.cpp
+++ six/modules/c++/six.sidd/unittests/test_valid_sixsidd.cpp
@@ -55,10 +55,10 @@ static inline std::filesystem::path six_sidd_relative_path()
 static std::filesystem::path schema_relative_path(const std::string& strVersion)
 {
     auto retval = six_sidd_relative_path() / "conf" / "schema"; // .../conf/schema
-    //if (strVersion == "3.0.0")
-    //{
-    //    retval = retval / "SIDD_V3.0.0"; // .../conf/schema/SIDD_V3.0.0
-    //}
+    if (strVersion == "3.0.0")
+    {
+        retval = retval / "SIDD_V3.0.0"; // .../conf/schema/SIDD_V3.0.0
+    }
     return retval;
 }
 
