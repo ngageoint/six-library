3754b22a150ca0e1854efc5c1766826280960fe2
Dan Smith <Dan.Smith@maxar.com>
2023-02-20 16:22:51 -0500

    Squashed 'externals/coda-oss/' changes from ba8547621..4c68fa0d3
    4c68fa0d3 fix directory names
    5ee2f039a poor-man's version of "import module std;" (#655)
    fad02842c Add HighFive library (#654)
    b31f5b6ab have Python 3.6 on a build server
    
    git-subtree-dir: externals/coda-oss
    git-subtree-split: 4c68fa0d34fbc53eb59de36ee3abd3a4978510bb

---
+++ modules/c++/std/include/import/cpp17.h
@@ -0,0 +1,36 @@
+/* =========================================================================
+ * This file is part of std-c++ 
+ * =========================================================================
+ * 
+ * (C) Copyright 2004 - 2014, MDA Information Systems LLC
+ * (C) Copyright 2023, Maxar Technologies, Inc.
+ *
+ * std-c++ is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program; If not, 
+ * see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef CODA_OSS_import_cpp17_h_INCLUDED_
+#define CODA_OSS_import_cpp17_h_INCLUDED_
+#pragma once
+
+#include "cpp14.h"
+
+// Common C++17 headers
+#include <filesystem>
+#include <optional>
+
+#endif // CODA_OSS_import_cpp17_h_INCLUDED_
+
+
