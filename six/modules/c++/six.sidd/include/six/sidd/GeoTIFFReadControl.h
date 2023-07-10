61095290d3cb6ae98537eb638c410452bea16553
Dan Smith <dan.smith@maxar.com>
2023-07-10 16:13:37 -0400

    somewhere a compiler warning got unsuppressed

--- six/modules/c++/six.sidd/include/six/sidd/GeoTIFFReadControl.h
+++ six/modules/c++/six.sidd/include/six/sidd/GeoTIFFReadControl.h
@@ -34,7 +34,9 @@ namespace sidd
 
 struct GeoTIFFReadControl : public ReadControl
 {
-    GeoTIFFReadControl() = default;
+    GeoTIFFReadControl() noexcept(false) {}
+
+    virtual ~GeoTIFFReadControl() noexcept = default;
 
     GeoTIFFReadControl(const GeoTIFFReadControl&) = delete;
     GeoTIFFReadControl operator=(const GeoTIFFReadControl&) = delete;
