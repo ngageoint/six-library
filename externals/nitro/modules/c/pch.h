4c03cc8dfa00379f7f4e5c3f2373ecf7c84dc364
J. Daniel Smith <dan@jdanielsmith.me>
2023-07-16 21:21:17 -0400

    fix NITF-C build errors

--- externals/nitro/modules/c/pch.h
+++ externals/nitro/modules/c/pch.h
@@ -3,20 +3,9 @@
 // We're building in Visual Studio ... used to control where we get a little bit of config info
 #define NITRO_PCH 1
 
-#include "config/disable_compiler_warnings.h"
-
 // TODO: get rid of these someday? ... from Visual Studio code-analysis
 #pragma warning(disable: 6385) // Reading invalid data from '...':  the readable size is '...' bytes, but '...' bytes may be read.
 #pragma warning(disable: 6386) // Buffer overrun while writing to '...':  the writable size is '...' bytes, but '...' bytes might be written.
-
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-#include <windows.h>
-#if defined(_WIN32)
-#undef BIGENDIAN
-#include <Winsock2.h>
-#else
-#include <netinet/in.h>
-#endif
 
 #include <assert.h>
 #include <stdlib.h>
@@ -29,13 +18,39 @@
 #include <memory.h>
 #include <stdint.h>
 
+#pragma warning(push)
+#pragma warning(disable: 4820) // '...': '...' bytes padding added after data member '...'
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+#if defined(_WIN32)
+#undef BIGENDIAN
+#include <Winsock2.h>
+#else
+#include <netinet/in.h>
+#endif
+
 #include <jpeglib.h>
 #include <jerror.h>
+
+#pragma warning(pop)
+
 
 #include <sys/types.h>
 
 #pragma warning(disable: 4505) // '...': unreferenced function with internal linkage has been removed
 #pragma warning(disable: 4774) // '...' : format string expected in argument 3 is not a string literal
+// We don't care about any padding added to structs
+#pragma warning(disable: 4820) //  '...': '...' bytes padding added after data member '...'
+
+// Assume any unreferenced functions will be used in other code
+#pragma warning(disable: 4514) // '...': unreferenced inline function has been removed
+
+// ???
+#pragma warning(disable: 4668) // '...' is not defined as a preprocessor macro, replacing with '...' for '...'
+
+// ???
+#pragma warning(disable: 5045) // Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
 
 #if __cplusplus
 #pragma warning(disable: 4365) // '...': conversion from '...' to '...', signed/unsigned mismatch
