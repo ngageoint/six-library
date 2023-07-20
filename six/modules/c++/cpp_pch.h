9791b106c94e2653f123a6a739a2fb413b7a6532
Dan Smith <Dan.Smith@maxar.com>
2023-07-20 15:23:47 -0400

    changes from cpp17

--- six/modules/c++/cpp_pch.h
+++ six/modules/c++/cpp_pch.h
@@ -43,6 +43,8 @@
 #include <atomic>
 #include <future>
 
+#pragma warning(disable: 5105) // macro expansion producing '...' has undefined behavior
+
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #define NOMINMAX
 #include <windows.h>
