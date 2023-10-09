d1c1c7836ff91a626c4f415103072640431615c8
Dan Smith <dan.smith@maxar.com>
2023-10-09 14:30:55 -0400

    except::Exception should be caught before std::exception

--- six/modules/c++/samples/test_create_sicd.cpp
+++ six/modules/c++/samples/test_create_sicd.cpp
@@ -284,15 +284,15 @@ int main(int argc, char** argv)
 
         return 0;
     }
-    catch (const std::exception& ex)
-    {
-        std::cerr << "Caught std::exception: " << ex.what() << std::endl;
-        return 1;
-    }
     catch (const except::Exception& ex)
     {
         std::cerr << "Caught except::Exception: " << ex.getMessage()
             << std::endl;
+        return 1;
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "Caught std::exception: " << ex.what() << std::endl;
         return 1;
     }
     catch (...)
