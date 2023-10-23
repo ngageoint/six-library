ccb71cb27518b1add75370441aa472763a5ba21b
Dan Smith <dan.smith@maxar.com>
2023-10-23 14:32:11 -0400

    Squashed 'externals/coda-oss/' changes from eb60210ff..16b7db37f
    16b7db37f Merge branch 'cpp17' into cpp20
    cfc7e73aa Merge branch 'cpp17' into cpp20
    f8335fa6d Merge branch 'cpp17' into cpp20
    422c334c8 Merge branch 'cpp17' into cpp20
    97f1dbf6d Merge branch 'cpp17' into cpp20
    e12fbce2e Merge branch 'cpp17' into cpp20
    729d61fb4 use GCC 10 on build server
    69e5f8d52 restore C++20
    
    git-subtree-dir: externals/coda-oss
    git-subtree-split: 16b7db37f701eac9aa036931b8ef9b8e31c08ad1

--- modules/c++/coda_oss/include/coda_oss/optional.h
+++ modules/c++/coda_oss/include/coda_oss/optional.h
@@ -22,34 +22,12 @@
 #ifndef CODA_OSS_coda_oss_optional_h_INCLUDED_
 #define CODA_OSS_coda_oss_optional_h_INCLUDED_
 
-#include "coda_oss/CPlusPlus.h"
-
-// This logic needs to be here rather than <std/optional> so that `coda_oss::optional` will
-// be the same as `std::optional`.
-#ifndef CODA_OSS_HAVE_std_optional_
-    #define CODA_OSS_HAVE_std_optional_ 0  // assume no <optional>
-#endif
-#if CODA_OSS_cpp17 // C++17 for `__has_include()`
-    #if __has_include(<optional>) // __cpp_lib_optional not until C++20
-        #include <optional>
-        #undef CODA_OSS_HAVE_std_optional_
-        #define CODA_OSS_HAVE_std_optional_ 1  // provided by the implementation, probably C++17
-    #endif
-#endif // CODA_OSS_cpp17
-
-#if !CODA_OSS_HAVE_std_optional_
-#include "coda_oss/optional_.h"
-#endif
+#include <optional>
 
 namespace coda_oss
 {
-    #if CODA_OSS_HAVE_std_optional_
-        using std::optional;
-        using std::make_optional;
-    #else
-        using details::optional;
-        using details::make_optional;
-    #endif 
+    using std::optional;
+    using std::make_optional;
 }
 
 #endif  // CODA_OSS_coda_oss_optional_h_INCLUDED_
